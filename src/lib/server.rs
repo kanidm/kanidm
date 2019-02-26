b729dc31c1f2231c01f09b2d129885a7e1a180d3
William Brown <william@blackhats.net.au>
2019-02-26 14:33:10 +1000

    Improve error handling in startup

--- src/lib/server.rs
+++ src/lib/server.rs
@@ -64,7 +64,9 @@ pub fn start(log: actix::Addr<EventLog>, path: &str, threads: usize) -> Result<a
                 //   Perhaps a config option to say if we index on startup or not.
                 // TODO: Check the results!
                 .and_then(|_| {
-                    schema_write.validate(&mut audit);
+                    schema_write.validate(&mut audit)
+                })
+                .and_then(|_| {
                     be_txn.commit()
                 })
                 .and_then(|_| {
