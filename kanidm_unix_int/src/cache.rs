40407ca1fb5f50d9b1101448ff97059095e76187
William Brown <william@blackhats.net.au>
2020-02-03 17:02:37 +1000

    Tests working

--- kanidm_unix_int/src/cache.rs
+++ kanidm_unix_int/src/cache.rs
@@ -1,5 +1,16 @@
+use std::time::{Duration, SystemTime};
 
-pub struct CacheLayer;
+#[derive(Debug)]
+enum CacheState {
+    Online,
+    Offline,
+    OfflineNextCheck(SystemTime),
+}
+
+#[derive(Debug)]
+pub struct CacheLayer {
+    state: CacheState
+}
 
 impl CacheLayer {
     pub fn new(
@@ -10,14 +21,33 @@ impl CacheLayer {
         // timeout
         // cache timeout
     ) -> Self {
-        CacheLayer
+        // We assume we are online at start up, which may change as we
+        // proceed.
+        CacheLayer {
+            state: CacheState::Online
+        }
     }
-    // Need a way to mark online/offline.
-    // What time do we re-check for online?
 
-    // Invalidate the whole cache
+    // Need a way to mark online/offline.
+    pub fn mark_online(&mut self) {
+        self.state = CacheState::Online;
+    }
+
+    pub fn mark_offline(&mut self) {
+        self.state = CacheState::Offline;
+    }
+
+    // Invalidate the whole cache. We do this by just deleting the content
+    // of the sqlite db.
+    pub fn invalidate(&self) {
+        unimplemented!();
+    }
 
     // Get ssh keys for an account id
+    pub async fn get_sshkeys(&mut self, account_id: &str) -> Result<Vec<String>, ()> {
+        unimplemented!();
+    }
+
     pub async fn test_async(&self) -> bool {
         true
     }
