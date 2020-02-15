6ad9f0bd591f0acbf7811831dde0135d911f1473
William Brown <william@blackhats.net.au>
2020-02-15 10:56:05 +1030

    fix constant

--- kanidm_unix_int/src/constants.rs
+++ kanidm_unix_int/src/constants.rs
@@ -1,4 +1,4 @@
-pub const DEFAULT_SOCK_PATH: &'static str = "/var/run/kanidm.sock";
-pub const DEFAULT_DB_PATH: &'static str = "/var/lib/kanidm/kanidm.cache.db";
+pub const DEFAULT_SOCK_PATH: &'static str = "/var/run/kanidm_unixd.sock";
+pub const DEFAULT_DB_PATH: &'static str = "/var/lib/kanidm_unixd/kanidm.cache.db";
 pub const DEFAULT_CONN_TIMEOUT: u64 = 2;
 pub const DEFAULT_CACHE_TIMEOUT: u64 = 15;
