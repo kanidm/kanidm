d46425c1ebf16570ccae62b19c3233b41a43c0ad
William Brown <william@blackhats.net.au>
2020-05-08 15:44:57 +1000

    Initial Refactor to move i64 to idl_sqlite

--- kanidmd/src/lib/be/mod.rs
+++ kanidmd/src/lib/be/mod.rs
@@ -17,6 +17,7 @@ use uuid::Uuid;
 
 pub mod dbentry;
 pub mod dbvalue;
+mod idl_arc_sqlite;
 mod idl_sqlite;
 
 use crate::be::idl_sqlite::{
@@ -33,9 +34,8 @@ pub enum IDL {
 }
 
 #[derive(Debug)]
-pub struct IdEntry {
-    // TODO #20: for now this is i64 to make sqlite work, but entry is u64 for indexing reasons!
-    id: i64,
+pub struct IdRawEntry {
+    id: u64,
     data: Vec<u8>,
 }
 
@@ -54,7 +54,7 @@ pub struct BackendWriteTransaction {
     idlayer: IdlSqliteWriteTransaction,
 }
 
-impl IdEntry {
+impl IdRawEntry {
     fn into_entry(self) -> Result<Entry<EntrySealed, EntryCommitted>, OperationError> {
         let db_e = serde_cbor::from_slice(self.data.as_slice())
             .map_err(|_| OperationError::SerdeCborError)?;
@@ -312,12 +312,7 @@ pub trait BackendTransaction {
             // Also get if the filter was 100% resolved or not.
             let idl = self.filter2idl(au, filt.to_inner(), FILTER_TEST_THRESHOLD)?;
 
-            let raw_entries = try_audit!(au, self.get_idlayer().get_identry(au, &idl));
-            let entries: Result<Vec<_>, _> = raw_entries
-                .into_iter()
-                .map(|ide| ide.into_entry())
-                .collect();
-            let entries = try_audit!(au, entries);
+            let entries = try_audit!(au, self.get_idlayer().get_identry(au, &idl));
             // Do other things
             // Now, de-serialise the raw_entries back to entries, and populate their ID's
 
@@ -377,12 +372,7 @@ pub trait BackendTransaction {
             match &idl {
                 IDL::Indexed(idl) => Ok(idl.len() > 0),
                 _ => {
-                    let raw_entries = try_audit!(au, self.get_idlayer().get_identry(au, &idl));
-                    let entries: Result<Vec<_>, _> = raw_entries
-                        .into_iter()
-                        .map(|ide| ide.into_entry())
-                        .collect();
-                    let entries = try_audit!(au, entries);
+                    let entries = try_audit!(au, self.get_idlayer().get_identry(au, &idl));
 
                     // if not 100% resolved query, apply the filter test.
                     let entries_filtered: Vec<_> = entries
@@ -405,7 +395,7 @@ pub trait BackendTransaction {
         // load all entries into RAM, may need to change this later
         // if the size of the database compared to RAM is an issue
         let idl = IDL::ALLIDS;
-        let raw_entries: Vec<IdEntry> = self.get_idlayer().get_identry(audit, &idl)?;
+        let raw_entries: Vec<IdRawEntry> = self.get_idlayer().get_identry_raw(audit, &idl)?;
 
         let entries: Result<Vec<DbEntry>, _> = raw_entries
             .iter()
@@ -483,22 +473,7 @@ impl BackendWriteTransaction {
                 })
                 .collect();
 
-            let identries: Result<Vec<_>, _> = c_entries
-                .iter()
-                .map(|e| {
-                    let dbe = e.to_dbentry();
-                    let data =
-                        serde_cbor::to_vec(&dbe).map_err(|_| OperationError::SerdeCborError)?;
-
-                    Ok(IdEntry {
-                        id: i64::try_from(e.get_id())
-                            .map_err(|_| OperationError::InvalidEntryID)?,
-                        data: data,
-                    })
-                })
-                .collect();
-
-            self.idlayer.write_identries(au, identries?)?;
+            self.idlayer.write_identries(au, c_entries.iter())?;
 
             // Now update the indexes as required.
             for e in c_entries.iter() {
@@ -525,13 +500,12 @@ impl BackendWriteTransaction {
 
         assert!(post_entries.len() == pre_entries.len());
 
+        /*
         // Assert the Id's exist on the entry, and serialise them.
         // Now, that means the ID must be > 0!!!
         let ser_entries: Result<Vec<IdEntry>, _> = post_entries
             .iter()
             .map(|e| {
-                let db_e = e.to_dbentry();
-
                 let id = i64::try_from(e.get_id())
                     .map_err(|_| OperationError::InvalidEntryID)
                     .and_then(|id| {
@@ -542,9 +516,7 @@ impl BackendWriteTransaction {
                         }
                     })?;
 
-                let data = serde_cbor::to_vec(&db_e).map_err(|_| OperationError::SerdeCborError)?;
-
-                Ok(IdEntry { id, data })
+                Ok(IdEntry { id, data: e.clone() })
             })
             .collect();
 
@@ -557,9 +529,10 @@ impl BackendWriteTransaction {
         if post_entries.len() != ser_entries.len() {
             return Err(OperationError::InvalidEntryState);
         }
+        */
 
         // Now, given the list of id's, update them
-        self.idlayer.write_identries(au, ser_entries)?;
+        self.idlayer.write_identries(au, post_entries.iter())?;
 
         // Finally, we now reindex all the changed entries. We do this by iterating and zipping
         // over the set, because we know the list is in the same order.
@@ -584,28 +557,8 @@ impl BackendWriteTransaction {
                 return Err(OperationError::EmptyRequest);
             }
 
-            // Assert the Id's exist on the entry.
-            let id_list: Result<Vec<i64>, _> = entries
-                .iter()
-                .map(|e| {
-                    i64::try_from(e.get_id())
-                        .map_err(|_| OperationError::InvalidEntryID)
-                        .and_then(|id| {
-                            if id == 0 {
-                                Err(OperationError::InvalidEntryID)
-                            } else {
-                                Ok(id)
-                            }
-                        })
-                })
-                .collect();
-
-            let id_list = try_audit!(au, id_list);
-
-            // Simple: If the list of id's is not the same as the input list, we are missing id's
-            if entries.len() != id_list.len() {
-                return Err(OperationError::InvalidEntryState);
-            }
+            // Assert the id's exist on the entry.
+            let id_list = entries.iter().map(|e| e.get_id());
 
             // Now, given the list of id's, delete them.
             self.idlayer.delete_identry(au, id_list)?;
@@ -754,12 +707,7 @@ impl BackendWriteTransaction {
         // Future idea: Do this in batches of X amount to limit memory
         // consumption.
         let idl = IDL::ALLIDS;
-        let raw_entries = try_audit!(audit, self.idlayer.get_identry(audit, &idl));
-        let entries: Result<Vec<_>, _> = raw_entries
-            .into_iter()
-            .map(|ide| ide.into_entry())
-            .collect();
-        let entries = try_audit!(audit, entries);
+        let entries = try_audit!(audit, self.idlayer.get_identry(audit, &idl));
 
         // WHEN do we update name2uuid and uuid2name?
         // Do they become attrs of the idx_cache? Should that be a struct?
@@ -847,18 +795,17 @@ impl BackendWriteTransaction {
 
         // Now, we setup all the entries with new ids.
         let mut id_max = 0;
-        let identries: Result<Vec<IdEntry>, _> = dbentries
+        let identries: Result<Vec<IdRawEntry>, _> = dbentries
             .iter()
-            .map(|ser_db_e| {
+            .map(|e| {
                 id_max += 1;
-                let data =
-                    serde_cbor::to_vec(&ser_db_e).map_err(|_| OperationError::SerdeCborError)?;
-
-                Ok(IdEntry { id: id_max, data })
+                let data = serde_cbor::to_vec(&e).map_err(|_| OperationError::SerdeCborError)?;
+                Ok(IdRawEntry { id: id_max, data })
             })
             .collect();
 
-        self.idlayer.write_identries(audit, identries?)?;
+        self.idlayer
+            .write_identries_raw(audit, identries?.into_iter())?;
 
         // for debug
         /*
