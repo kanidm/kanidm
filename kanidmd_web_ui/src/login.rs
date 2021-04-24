518a0e369b647083c57ba4b70cb99d9638cc396d
James Hodgkinson <james@terminaloutcomes.com>
2021-04-24 14:27:57 +1000

    blep

--- kanidmd_web_ui/src/login.rs
+++ kanidmd_web_ui/src/login.rs
@@ -20,6 +20,7 @@ extern "C" {
 pub struct LoginApp {
     link: ComponentLink<Self>,
     inputvalue: String,
+    #[allow(dead_code)]
     lstorage: StorageService,
     ft: Option<FetchTask>,
     session_id: String,
@@ -262,7 +263,7 @@ impl Component for LoginApp {
     type Properties = ();
 
     fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
-        ConsoleService::log(format!("create").as_str());
+        ConsoleService::log(&"create".to_string());
 
         // First we need to work out what state we are in.
         let lstorage = StorageService::new(yew::services::storage::Area::Local).unwrap();
