c1b90fd95cc7d21a4d4430e4a79622d29f44ec14
James Hodgkinson <james@terminaloutcomes.com>
2021-08-01 12:27:29 +1000

    changing to combineable flags

--- kanidmd/src/lib/constants/ldap.rs
+++ kanidmd/src/lib/constants/ldap.rs
@@ -1,45 +1,22 @@
 //! Constants for the LDAP server
 
+use std::convert::From;
+use std::ops::BitAnd;
+
 /// Flags and byte values for the LDAP UserAccountControl Attribute.
 /// This attribute value can be zero or a combination of one or more of the following values.
 ///
-/// The flags are cumulative. To disable a user's account, set the attribute to 514 (2 + 512).
+/// The flags are cumulative. To disable a user's account, set the attribute to [LdapUacFlag::AccountDisable] + [LdapUacFlag::NormalAccount]  (2 + 512).
 ///
 /// Since User-Account-Control-Attribute is a constructed attribute, it cannot be used in an LDAP search filter.
 ///
 /// Ref: <https://ldapwiki.com/wiki/User-Account-Control%20Attribute%20Values>
-#[derive(Default)]
-pub struct LdapUac {
-    pub value: u32,
-}
 
-impl LdapUac {
-    pub fn new() -> Self {
-        LdapUac { value: 0 }
-    }
-
-    pub fn flag_disable(self) -> Self {
-        LdapUac {
-            value: self.value + LdapUacFlag::AccountDisable.to_uint(),
-        }
-    }
-
-    pub fn flag_normal_account(self) -> Self {
-        LdapUac {
-            value: self.value + LdapUacFlag::NormalAccount.to_uint(),
-        }
-    }
-
-    /// Add an LdapUacFlag to a LdapUac object
-    pub fn addflag(self, newflag: &LdapUacFlag) -> Self {
-        LdapUac {
-            value: self.value + newflag.to_uint(),
-        }
-    }
-}
-
-/// Flag values for the LdapUac value.
+#[derive(Debug)]
+/// Flag values for UserAccountControl. AND (&) them together to get a flag value to return in LDAP responses
 pub enum LdapUacFlag {
+    /// If you provide it a wrong value, you get back InvalidFlag which equates to 0;
+    InvalidFlag,
     /// The logon script is executed.
     Script,
     /// The user account is disabled.
@@ -88,10 +65,17 @@ pub enum LdapUacFlag {
     UserUseAesKeys,
 }
 
-impl LdapUacFlag {
+impl From<LdapUacFlag> for u32 {
     /// Get the uint type of the flag, so you can add them together.
-    fn to_uint(&self) -> u32 {
-        match self {
+    ///
+    /// ```
+    /// use kanidm::constants::ldap::LdapUacFlag;
+    /// let testuac: u32 = LdapUacFlag::UserUseAesKeys.into();
+    /// assert_eq!(testuac, 2147483648);
+    /// ```
+    fn from(flag: LdapUacFlag) -> u32 {
+        match flag {
+            LdapUacFlag::InvalidFlag => 0,
             LdapUacFlag::Script => 1,
             LdapUacFlag::AccountDisable => 2,
             LdapUacFlag::HomedirRequired => 8,
@@ -119,30 +103,86 @@ impl LdapUacFlag {
     }
 }
 
-mod tests {
-    #[test]
-    fn test_ldap_uac_adding_flags() {
-        use crate::constants::ldap::{LdapUac, LdapUacFlag};
-
-        let testuac = LdapUac::new();
-        assert_eq!(0, testuac.value);
-
-        let testuac = testuac.addflag(&LdapUacFlag::Lockout);
-        assert_eq!(16, testuac.value);
+impl BitAnd for LdapUacFlag {
+    type Output = u32;
+    /// Implements the & function for [LdapUacFlag]
+    ///
+    /// ```
+    /// use kanidm::constants::ldap::LdapUacFlag;
+    /// let testval: u32 = LdapUacFlag::Lockout & LdapUacFlag::PasswdNotRequired;
+    /// assert_eq!(testval, 48);
+    /// ```
+    fn bitand(self, rhs: Self) -> u32 {
+        let lhs: u32 = self.into();
+        let rhs: u32 = rhs.into();
+        lhs & rhs
     }
+}
 
-    #[test]
-    fn test_ldap_uac_fromstring() {
-        use crate::constants::ldap::LdapUacFlag;
-        let testuac = LdapUacFlag::UserUseAesKeys;
-        assert_eq!(testuac.to_uint(), 2147483648);
+impl BitAnd<u32> for LdapUacFlag {
+    type Output = u32;
+
+    /// Implements the & function for [LdapUacFlag]
+    ///
+    /// ```
+    /// use kanidm::constants::ldap::LdapUacFlag;
+    /// let testval: u32 = LdapUacFlag::Lockout & LdapUacFlag::PasswdNotRequired;
+    /// assert_eq!(testval, 48);
+    /// ```
+    fn bitand(self, rhs: u32) -> u32 {
+        // rhs is the "right-hand side" of the expression `a & b`
+        let lhs: u32 = self.into();
+        let rhs: u32 = rhs;
+        lhs & rhs
     }
-    #[test]
-    fn test_ldap_uac_normalaccount() {
-        use crate::constants::ldap::LdapUac;
-        let user = LdapUac::new().flag_normal_account();
-        assert_eq!(user.value, 512);
-        let user = LdapUac::new().flag_normal_account().flag_disable();
-        assert_eq!(user.value, 514);
+}
+
+impl From<&LdapUacFlag> for u32 {
+    fn from(flag: &LdapUacFlag) -> u32 {
+        let result: u32 = flag.into();
+        result
+    }
+}
+
+impl From<u32> for LdapUacFlag {
+    /// Returns a LdapUacFlag if you give it a u32
+    ///
+    /// ```
+    /// use kanidm::constants::ldap::LdapUacFlag;
+    /// let lhs: u32 = LdapUacFlag::NotDelgated.into();
+    /// let rhs: u32 = LdapUacFlag::from(1048576).into();
+    ///
+    /// assert_eq!(lhs, rhs);
+    /// ```
+    fn from(number: u32) -> Self {
+        match number {
+            1 => LdapUacFlag::Script,
+            2 => LdapUacFlag::AccountDisable,
+            8 => LdapUacFlag::HomedirRequired,
+            16 => LdapUacFlag::Lockout,
+            32 => LdapUacFlag::PasswdNotRequired,
+            64 => LdapUacFlag::PasswordCantChange,
+            128 => LdapUacFlag::EncryptedTextPasswordAllowed,
+            256 => LdapUacFlag::TemplDuplicateAccount,
+            512 => LdapUacFlag::NormalAccount,
+            2048 => LdapUacFlag::InterDomainTrustAccount,
+            4096 => LdapUacFlag::WorkstationTrustAccount,
+            8192 => LdapUacFlag::ServerTrustAccount,
+            65536 => LdapUacFlag::DontExpirePassword,
+            131072 => LdapUacFlag::MnsLogonAccount,
+            262144 => LdapUacFlag::SmartcardRequired,
+            524288 => LdapUacFlag::TrustedForDelegation,
+            1048576 => LdapUacFlag::NotDelgated,
+            2097152 => LdapUacFlag::UseDesKeyOnly,
+            4194304 => LdapUacFlag::DontRequirePreAuth,
+            8388608 => LdapUacFlag::ErrorPasswordExpired,
+            16777216 => LdapUacFlag::TrustedToAuthnForDelegation,
+            67108864 => LdapUacFlag::PartialSecretsAccount,
+            2147483648 => LdapUacFlag::UserUseAesKeys,
+            _ => {
+                eprintln!("Invalid flag int provided, returning InvalidFlag");
+                LdapUacFlag::InvalidFlag
+            }
+        }
     }
 }
