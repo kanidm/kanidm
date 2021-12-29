aa75a696ec76c8866ebeb9cd1b3e56354edddd9a
Firstyear <william@blackhats.net.au>
2021-12-29 11:36:56 +1000

    Improve autofocus to oauth2 (#630)

--- kanidmd_web_ui/build_wasm.sh
+++ kanidmd_web_ui/build_wasm.sh
@@ -2,6 +2,7 @@
 wasm-pack build --no-typescript --release --target web && \
     rollup ./src/main.js --format iife --file ./pkg/bundle.js && \
     cp ./src/style.css ./pkg/style.css && \
+    cp -a ./src/external ./pkg/external && \
     rm ./pkg/.gitignore
     
 
