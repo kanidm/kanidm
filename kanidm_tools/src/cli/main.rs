kanidm/kanidm

commit 3f7c692a7c5b54505b317b39be78d0d40aaafd8e

Cleanup display output (#659)

kanidm_tools/src/cli/main.rs
@@ -13,16 +13,33 @@
 use kanidm_cli::KanidmClientOpt;
 use structopt::StructOpt;
 
+use tracing_subscriber::prelude::*;
+use tracing_subscriber::{fmt, EnvFilter};
+
 fn main() {
     let opt = KanidmClientOpt::from_args();
 
-    if opt.debug() {
-        ::std::env::set_var(
-            "RUST_LOG",
-            "kanidm=debug,kanidm_client=debug,webauthn=debug",
-        );
-    }
-    tracing_subscriber::fmt::init();
+    let fmt_layer = fmt::layer().with_writer(std::io::stderr);
+
+    let filter_layer = if opt.debug() {
+        match EnvFilter::try_new("kanidm=debug,kanidm_client=debug,webauthn=debug") {
+            Ok(f) => f,
+            Err(e) => {
+                eprintln!("ERROR! Unable to start tracing {:?}", e);
+                return;
+            }
+        }
+    } else {
+        match EnvFilter::try_from_default_env() {
+            Ok(f) => f,
+            Err(_) => EnvFilter::new("kanidm_client=warn"),
+        }
+    };
+
+    tracing_subscriber::registry()
+        .with(filter_layer)
+        .with(fmt_layer)
+        .init();
 
     opt.exec()
 }

kanidm_tools/Cargo.toml
@@ -31,7 +31,7 @@ path = "src/badlist_preprocess.rs"
 kanidm_client = { path = "../kanidm_client", version = "1.1.0-alpha.4" }
 kanidm_proto = { path = "../kanidm_proto", version = "1.1.0-alpha.4" }
 tracing = "0.1"
-tracing-subscriber = "0.3"
+tracing-subscriber = { version = "0.3", features = ["env-filter", "fmt"] }
 rpassword = "5.0"
 structopt = { version = "0.3", default-features = false }
 libc = "0.2"

kanidm_tools/src/cli/account.rs
@@ -100,7 +100,7 @@ impl AccountOpt {
                             );
                         }
                         Err(e) => {
-                            eprintln!("Error -> {:?}", e);
+                            error!("Error -> {:?}", e);
                         }
                     }
                 }
@@ -114,7 +114,7 @@ impl AccountOpt {
                         ) {
                         Ok(v) => v,
                         Err(e) => {
-                            eprintln!("Error Starting Registration -> {:?}", e);
+                            error!("Error Starting Registration -> {:?}", e);
                             return;
                         }
                     };
@@ -126,7 +126,7 @@ impl AccountOpt {
                     let rego = match wa.do_registration(client.get_origin(), chal) {
                         Ok(rego) => rego,
                         Err(e) => {
-                            eprintln!("Error Signing -> {:?}", e);
+                            error!("Error Signing -> {:?}", e);
                             return;
                         }
                     };
@@ -140,7 +140,7 @@ impl AccountOpt {
                             println!("Webauthn token registration success.");
                         }
                         Err(e) => {
-                            eprintln!("Error Completing -> {:?}", e);
+                            error!("Error Completing -> {:?}", e);
                         }
                     }
                 }
@@ -154,7 +154,7 @@ impl AccountOpt {
                             println!("Webauthn removal success.");
                         }
                         Err(e) => {
-                            eprintln!("Error Removing Webauthn from account -> {:?}", e);
+                            error!("Error Removing Webauthn from account -> {:?}", e);
                         }
                     }
                 }
@@ -165,7 +165,7 @@ impl AccountOpt {
                     ) {
                         Ok(v) => v,
                         Err(e) => {
-                            eprintln!("Error Starting Registration -> {:?}", e);
+                            error!("Error Starting Registration -> {:?}", e);
                             return;
                         }
                     };
@@ -176,7 +176,7 @@ impl AccountOpt {
                     let code = match QrCode::new(tok.to_uri().as_str()) {
                         Ok(c) => c,
                         Err(e) => {
-                            eprintln!("Failed to generate QR code -> {:?}", e);
+                            error!("Failed to generate QR code -> {:?}", e);
                             return;
                         }
                     };
@@ -206,7 +206,7 @@ impl AccountOpt {
                         // Finish the line?
                         eprintln!();
                         if let Err(e) = input_result {
-                            eprintln!("Failed to read from stdin -> {:?}", e);
+                            error!("Failed to read from stdin -> {:?}", e);
                             break;
                         };
 
@@ -214,7 +214,7 @@ impl AccountOpt {
                         let totp = match totp_input.trim().parse::<u32>() {
                             Ok(v) => v,
                             Err(e) => {
-                                eprintln!("Invalid TOTP -> {:?}", e);
+                                error!("Invalid TOTP -> {:?}", e);
                                 // Try again.
                                 continue;
                             }
@@ -238,7 +238,7 @@ impl AccountOpt {
 
                                 let mut confirm_input = String::new();
                                 if let Err(e) = io::stdin().read_line(&mut confirm_input) {
-                                    eprintln!("Failed to read from stdin -> {:?}", e);
+                                    error!("Failed to read from stdin -> {:?}", e);
                                 };
 
                                 if confirm_input.to_lowercase().trim() == "i am sure" {
@@ -250,7 +250,7 @@ impl AccountOpt {
                                             println!("TOTP registration success.");
                                         }
                                         Err(e) => {
-                                            eprintln!("Error Completing -> {:?}", e);
+                                            error!("Error Completing -> {:?}", e);
                                         }
                                     };
                                 } else {
@@ -264,7 +264,7 @@ impl AccountOpt {
                                 continue;
                             }
                             Err(e) => {
-                                eprintln!("Error Completing -> {:?}", e);
+                                error!("Error Completing -> {:?}", e);
                                 break;
                             }
                         }
@@ -279,7 +279,7 @@ impl AccountOpt {
                             println!("TOTP removal success.");
                         }
                         Err(e) => {
-                            eprintln!("Error Removing TOTP from account -> {:?}", e);
+                            error!("Error Removing TOTP from account -> {:?}", e);
                         }
                     }
                 }
@@ -295,7 +295,7 @@ impl AccountOpt {
                             println!("---");
                         }
                         Err(e) => {
-                            eprintln!("Error generating Backup Codes for account -> {:?}", e);
+                            error!("Error generating Backup Codes for account -> {:?}", e);
                         }
                     }
                 }
@@ -308,7 +308,7 @@ impl AccountOpt {
                             println!("BackupCodeRemove success.");
                         }
                         Err(e) => {
-                            eprintln!("Error BackupCodeRemove for account -> {:?}", e);
+                            error!("Error BackupCodeRemove for account -> {:?}", e);
                         }
                     }
                 }
@@ -320,7 +320,7 @@ impl AccountOpt {
                             print!("{}", status);
                         }
                         Err(e) => {
-                            eprintln!("Error displaying credential status -> {:?}", e);
+                            error!("Error displaying credential status -> {:?}", e);
                         }
                     }
                 }
@@ -336,7 +336,7 @@ impl AccountOpt {
                         Ok(Some(s)) => println!("Radius secret: {}", s),
                         Ok(None) => println!("NO Radius secret"),
                         Err(e) => {
-                            eprintln!("Error -> {:?}", e);
+                            error!("Error -> {:?}", e);
                         }
                     }
                 }
@@ -345,7 +345,7 @@ impl AccountOpt {
                     if let Err(e) = client
                         .idm_account_radius_credential_regenerate(aopt.aopts.account_id.as_str())
                     {
-                        eprintln!("Error -> {:?}", e);
+                        error!("Error -> {:?}", e);
                     }
                 }
                 AccountRadius::Delete(aopt) => {
@@ -353,7 +353,7 @@ impl AccountOpt {
                     if let Err(e) =
                         client.idm_account_radius_credential_delete(aopt.aopts.account_id.as_str())
                     {
-                        eprintln!("Error -> {:?}", e);
+                        error!("Error -> {:?}", e);
                     }
                 }
             }, // end AccountOpt::Radius
@@ -363,7 +363,7 @@ impl AccountOpt {
                     match client.idm_account_unix_token_get(aopt.aopts.account_id.as_str()) {
                         Ok(token) => println!("{}", token),
                         Err(e) => {
-                            eprintln!("Error -> {:?}", e);
+                            error!("Error -> {:?}", e);
                         }
                     }
                 }
@@ -374,7 +374,7 @@ impl AccountOpt {
                         aopt.gidnumber,
                         aopt.shell.as_deref(),
                     ) {
-                        eprintln!("Error -> {:?}", e);
+                        error!("Error -> {:?}", e);
                     }
                 }
                 AccountPosix::SetPassword(aopt) => {
@@ -391,7 +391,7 @@ impl AccountOpt {
                         aopt.aopts.account_id.as_str(),
                         password.as_str(),
                     ) {
-                        eprintln!("Error -> {:?}", e);
+                        error!("Error -> {:?}", e);
                     }
                 }
             }, // end AccountOpt::Posix
@@ -403,7 +403,7 @@ impl AccountOpt {
                         aopt.mail.as_deref(),
                         aopt.legalname.as_deref(),
                     ) {
-                        eprintln!("Error -> {:?}", e);
+                        error!("Error -> {:?}", e);
                     }
                 }
             }, // end AccountOpt::Person
@@ -414,7 +414,7 @@ impl AccountOpt {
                     match client.idm_account_get_ssh_pubkeys(aopt.aopts.account_id.as_str()) {
                         Ok(pkeys) => pkeys.iter().for_each(|pkey| println!("{}", pkey)),
                         Err(e) => {
-                            eprintln!("Error -> {:?}", e);
+                            error!("Error -> {:?}", e);
                         }
                     }
                 }
@@ -425,7 +425,7 @@ impl AccountOpt {
                         aopt.tag.as_str(),
                         aopt.pubkey.as_str(),
                     ) {
-                        eprintln!("Error -> {:?}", e);
+                        error!("Error -> {:?}", e);
                     }
                 }
                 AccountSsh::Delete(aopt) => {
@@ -434,7 +434,7 @@ impl AccountOpt {
                         aopt.aopts.account_id.as_str(),
                         aopt.tag.as_str(),
                     ) {
-                        eprintln!("Error -> {:?}", e);
+                        error!("Error -> {:?}", e);
                     }
                 }
             }, // end AccountOpt::Ssh
@@ -442,7 +442,7 @@ impl AccountOpt {
                 let client = copt.to_client();
                 match client.idm_account_list() {
                     Ok(r) => r.iter().for_each(|ent| println!("{}", ent)),
-                    Err(e) => eprintln!("Error -> {:?}", e),
+                    Err(e) => error!("Error -> {:?}", e),
                 }
             }
             AccountOpt::Get(aopt) => {
@@ -450,13 +450,13 @@ impl AccountOpt {
                 match client.idm_account_get(aopt.aopts.account_id.as_str()) {
                     Ok(Some(e)) => println!("{}", e),
                     Ok(None) => println!("No matching entries"),
-                    Err(e) => eprintln!("Error -> {:?}", e),
+                    Err(e) => error!("Error -> {:?}", e),
                 }
             }
             AccountOpt::Delete(aopt) => {
                 let client = aopt.copt.to_client();
                 if let Err(e) = client.idm_account_delete(aopt.aopts.account_id.as_str()) {
-                    eprintln!("Error -> {:?}", e)
+                    error!("Error -> {:?}", e)
                 }
             }
             AccountOpt::Create(acopt) => {
@@ -465,7 +465,7 @@ impl AccountOpt {
                     acopt.aopts.account_id.as_str(),
                     acopt.display_name.as_str(),
                 ) {
-                    eprintln!("Error -> {:?}", e)
+                    error!("Error -> {:?}", e)
                 }
             }
             AccountOpt::Validity(avopt) => match avopt {
@@ -517,7 +517,7 @@ impl AccountOpt {
                                 println!("expire: never");
                             }
                         }
-                        Err(e) => eprintln!("Error -> {:?}", e),
+                        Err(e) => error!("Error -> {:?}", e),
                     }
                 }
                 AccountValidity::ExpireAt(ano) => {
@@ -527,14 +527,14 @@ impl AccountOpt {
                         match client
                             .idm_account_purge_attr(ano.aopts.account_id.as_str(), "account_expire")
                         {
-                            Err(e) => eprintln!("Error -> {:?}", e),
+                            Err(e) => error!("Error -> {:?}", e),
                             _ => println!("Success"),
                         }
                     } else {
                         if let Err(e) =
                             OffsetDateTime::parse(ano.datetime.as_str(), time::Format::Rfc3339)
                         {
-                            eprintln!("Error -> {:?}", e);
+                            error!("Error -> {:?}", e);
                             return;
                         }
 
@@ -543,7 +543,7 @@ impl AccountOpt {
                             "account_expire",
                             &[ano.datetime.as_str()],
                         ) {
-                            Err(e) => eprintln!("Error -> {:?}", e),
+                            Err(e) => error!("Error -> {:?}", e),
                             _ => println!("Success"),
                         }
                     }
@@ -556,7 +556,7 @@ impl AccountOpt {
                             ano.aopts.account_id.as_str(),
                             "account_valid_from",
                         ) {
-                            Err(e) => eprintln!("Error -> {:?}", e),
+                            Err(e) => error!("Error -> {:?}", e),
                             _ => println!("Success"),
                         }
                     } else {
@@ -564,7 +564,7 @@ impl AccountOpt {
                         if let Err(e) =
                             OffsetDateTime::parse(ano.datetime.as_str(), time::Format::Rfc3339)
                         {
-                            eprintln!("Error -> {:?}", e);
+                            error!("Error -> {:?}", e);
                             return;
                         }
 
@@ -573,7 +573,7 @@ impl AccountOpt {
                             "account_valid_from",
                             &[ano.datetime.as_str()],
                         ) {
-                            Err(e) => eprintln!("Error -> {:?}", e),
+                            Err(e) => error!("Error -> {:?}", e),
                             _ => println!("Success"),
                         }
                     }

kanidm_tools/src/cli/common.rs
@@ -87,7 +87,7 @@ impl CommonOpt {
                     match prompt_for_username_get_token() {
                         Ok(value) => value,
                         Err(msg) => {
-                            eprintln!("{}", msg);
+                            error!("{}", msg);
                             std::process::exit(1);
                         }
                     }
@@ -98,7 +98,7 @@ impl CommonOpt {
         let jwtu = match JwsUnverified::from_str(&token) {
             Ok(jwtu) => jwtu,
             Err(e) => {
-                eprintln!("Unable to parse token - {:?}", e);
+                error!("Unable to parse token - {:?}", e);
                 std::process::exit(1);
             }
         };
@@ -139,7 +139,7 @@ pub fn prompt_for_username_get_values() -> Result<(String, String), String> {
         _ => return Err("Error retrieving authentication token store".to_string()),
     };
     if tokens.is_empty() {
-        eprintln!("No tokens in store, quitting!");
+        error!("No tokens in store, quitting!");
         std::process::exit(1);
     }
     let mut options = Vec::new();
@@ -153,7 +153,7 @@ pub fn prompt_for_username_get_values() -> Result<(String, String), String> {
         .interact();
     let selection = match user_select {
         Err(error) => {
-            eprintln!("Failed to handle user input: {:?}", error);
+            error!("Failed to handle user input: {:?}", error);
             std::process::exit(1);
         }
         Ok(value) => value,
@@ -168,7 +168,7 @@ pub fn prompt_for_username_get_values() -> Result<(String, String), String> {
             Ok((f_uname.to_string(), f_token.to_string()))
         }
         None => {
-            eprintln!("Memory corruption trying to read token store, quitting!");
+            error!("Memory corruption trying to read token store, quitting!");
             std::process::exit(1);
         }
     }

kanidm_tools/src/cli/domain.rs
@@ -13,14 +13,14 @@ impl DomainOpt {
                 let client = copt.to_client();
                 match client.idm_domain_get() {
                     Ok(e) => println!("{}", e),
-                    Err(e) => eprintln!("Error -> {:?}", e),
+                    Err(e) => error!("Error -> {:?}", e),
                 }
             }
             DomainOpt::ResetTokenKey(copt) => {
                 let client = copt.to_client();
                 match client.idm_domain_reset_token_key() {
                     Ok(_) => println!("Success"),
-                    Err(e) => eprintln!("Error -> {:?}", e),
+                    Err(e) => error!("Error -> {:?}", e),
                 }
             }
         }

kanidm_tools/src/cli/group.rs
@@ -24,7 +24,7 @@ impl GroupOpt {
             GroupOpt::List(copt) => {
                 let client = copt.to_client();
                 match client.idm_group_list() {
-                    Ok(r) => r.iter().for_each(|ent| info!("{}", ent)),
+                    Ok(r) => r.iter().for_each(|ent| println!("{}", ent)),
                     Err(e) => error!("Error -> {:?}", e),
                 }
             }
@@ -32,8 +32,8 @@ impl GroupOpt {
                 let client = gcopt.copt.to_client();
                 // idm_group_get
                 match client.idm_group_get(gcopt.name.as_str()) {
-                    Ok(Some(e)) => info!("{}", e),
-                    Ok(None) => info!("No matching group '{}'", gcopt.name.as_str()),
+                    Ok(Some(e)) => println!("{}", e),
+                    Ok(None) => warn!("No matching group '{}'", gcopt.name.as_str()),
                     Err(e) => error!("Error -> {:?}", e),
                 }
             }
@@ -41,21 +41,21 @@ impl GroupOpt {
                 let client = gcopt.copt.to_client();
                 match client.idm_group_create(gcopt.name.as_str()) {
                     Err(e) => error!("Error -> {:?}", e),
-                    Ok(_) => info!("Successfully created group '{}'", gcopt.name.as_str()),
+                    Ok(_) => println!("Successfully created group '{}'", gcopt.name.as_str()),
                 }
             }
             GroupOpt::Delete(gcopt) => {
                 let client = gcopt.copt.to_client();
                 match client.idm_group_delete(gcopt.name.as_str()) {
                     Err(e) => error!("Error -> {:?}", e),
-                    Ok(_) => info!("Successfully deleted group {}", gcopt.name.as_str()),
+                    Ok(_) => println!("Successfully deleted group {}", gcopt.name.as_str()),
                 }
             }
             GroupOpt::PurgeMembers(gcopt) => {
                 let client = gcopt.copt.to_client();
                 match client.idm_group_purge_members(gcopt.name.as_str()) {
                     Err(e) => error!("Error -> {:?}", e),
-                    Ok(_) => info!(
+                    Ok(_) => println!(
                         "Successfully purged members of group {}",
                         gcopt.name.as_str()
                     ),
@@ -64,8 +64,8 @@ impl GroupOpt {
             GroupOpt::ListMembers(gcopt) => {
                 let client = gcopt.copt.to_client();
                 match client.idm_group_get_members(gcopt.name.as_str()) {
-                    Ok(Some(groups)) => groups.iter().for_each(|m| info!("{:?}", m)),
-                    Ok(None) => info!("No members in group {}", gcopt.name.as_str()),
+                    Ok(Some(groups)) => groups.iter().for_each(|m| println!("{:?}", m)),
+                    Ok(None) => warn!("No members in group {}", gcopt.name.as_str()),
                     Err(e) => error!("Error -> {:?}", e),
                 }
             }
@@ -75,7 +75,7 @@ impl GroupOpt {
 
                 match client.idm_group_add_members(gcopt.name.as_str(), &new_members) {
                     Err(e) => error!("Error -> {:?}", e),
-                    Ok(_) => info!("Successfully added members to {}", gcopt.name.as_str()),
+                    Ok(_) => warn!("Successfully added members to {}", gcopt.name.as_str()),
                 }
             }
 
@@ -85,7 +85,7 @@ impl GroupOpt {
 
                 match client.idm_group_remove_members(gcopt.name.as_str(), &remove_members) {
                     Err(e) => error!("Failed to remove members -> {:?}", e),
-                    Ok(_) => info!("Successfully removed members from {}", gcopt.name.as_str()),
+                    Ok(_) => println!("Successfully removed members from {}", gcopt.name.as_str()),
                 }
             }
 
@@ -95,14 +95,14 @@ impl GroupOpt {
 
                 match client.idm_group_set_members(gcopt.name.as_str(), &new_members) {
                     Err(e) => error!("Error -> {:?}", e),
-                    Ok(_) => info!("Successfully set members for group {}", gcopt.name.as_str()),
+                    Ok(_) => println!("Successfully set members for group {}", gcopt.name.as_str()),
                 }
             }
             GroupOpt::Posix(gpopt) => match gpopt {
                 GroupPosix::Show(gcopt) => {
                     let client = gcopt.copt.to_client();
                     match client.idm_group_unix_token_get(gcopt.name.as_str()) {
-                        Ok(token) => info!("{}", token),
+                        Ok(token) => println!("{}", token),
                         Err(e) => error!("Error -> {:?}", e),
                     }
                 }
@@ -110,7 +110,7 @@ impl GroupOpt {
                     let client = gcopt.copt.to_client();
                     match client.idm_group_unix_extend(gcopt.name.as_str(), gcopt.gidnumber) {
                         Err(e) => error!("Error -> {:?}", e),
-                        Ok(_) => info!(
+                        Ok(_) => println!(
                             "Success adding POSIX configuration for group {}",
                             gcopt.name.as_str()
                         ),

commit a8ed03685c0b388f0647cd7e787fde3baf6c1580

Display errors when cli operations fail
```
# before this change
$ kanidm account delete does-not-exist -D admin
$ # no output; exit status 0

# after this change
$ kanidm account delete does-not-exist -D admin                                                                                                                                        5s
2022-04-01T21:34:51.857904Z ERROR kanidm_cli::account: Error -> Http(404, Some(NoMatchingEntries), "32a34959-2cc9-449e-b9c3-dff8d30a736b")
```

This fixes a regression, introduced in #659, in the CLI's default output.
Since 404 & 403 errors don't trigger any logs further down the stack,
they'd exit eerily silently with the default cli loglevel.

kanidm_tools/src/cli/main.rs
@@ -22,7 +22,8 @@ fn main() {
     let fmt_layer = fmt::layer().with_writer(std::io::stderr);
 
     let filter_layer = if opt.debug() {
-        match EnvFilter::try_new("kanidm=debug,kanidm_client=debug,webauthn=debug") {
+        match EnvFilter::try_new("kanidm=debug,kanidm_client=debug,webauthn=debug,kanidm_cli=debug")
+        {
             Ok(f) => f,
             Err(e) => {
                 eprintln!("ERROR! Unable to start tracing {:?}", e);
@@ -32,7 +33,7 @@ fn main() {
     } else {
         match EnvFilter::try_from_default_env() {
             Ok(f) => f,
-            Err(_) => EnvFilter::new("kanidm_client=warn"),
+            Err(_) => EnvFilter::new("kanidm_client=warn,kanidm_cli=warn"),
         }
     };
 