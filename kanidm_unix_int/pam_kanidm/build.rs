5d799ad3e9074ccd9a31663a30220c1b4c446e55
Euan Kemp <euank@euank.com>
2022-04-01 13:45:19 -0700

    Use pkg-config to link against pam
    Some distros, such as nixos, require more than just '-lpam' to locate
    the library. Adding a naive pkg-config invocation to the pam FFI
    crate allows pam_kanidm to build on my system, where before this change
    it did not.

---
+++ kanidm_unix_int/pam_kanidm/build.rs
@@ -0,0 +1,8 @@
+fn main() {
+    // ignore errors here since older versions of pam do not ship the pkg-config `pam.pc` file.
+    // Not setting anything here will fall back on just blindly linking with `-lpam`,
+    // which will work on environments with libpam.so, but no pkg-config file.
+    let _ = pkg_config::Config::new()
+        .atleast_version("1.3.0")
+        .probe("pam");
+}
