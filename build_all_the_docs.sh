b90a7b1d8aa09e728ac0729162b388ccb01e8cd3
James Hodgkinson <james@terminaloutcomes.com>
2022-04-27 20:11:08 +1000

    updatey scripty boopydoopy

--- build_all_the_docs.sh
+++ build_all_the_docs.sh
@@ -8,7 +8,7 @@ function build_version() {
     echo "Book version: ${BOOK_VERSION}"
 	git switch -c "${BOOK_VERSION}"
 	git pull origin "${BOOK_VERSION}"
-	cargo doc --no-deps --quiet
+	RUSTFLAGS=-Awarnings cargo doc --quiet --no-deps
 	mdbook build kanidm_book
 	mv ./kanidm_book/book/ "${DOCS_DIR}/${BOOK_VERSION}/"
 	mkdir -p "${DOCS_DIR}/${BOOK_VERSION}/rustdoc/"
@@ -17,7 +17,9 @@ function build_version() {
 
 mkdir -p "${DOCS_DIR}"
 
-for version in $(git tag -l | grep -E '^v' | head -n2); do
+build_version master
+
+for version in $(git tag -l 'v*' --sort "-version:refname" | grep -v '1.1.0alpha'); do
     echo "$version"
     build_version "${version}"
 done
