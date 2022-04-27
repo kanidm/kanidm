5a9b5e0f206a381314b503cbb60b1fb813f6a383
Firstyear <Firstyear@users.noreply.github.com>
2022-04-27 01:00:53 +0000

    deploy: 9ade567a52cf412f52ad79855a311a19c3ccf8bd

--- docs/rustdoc/master/kanidm/tracing_tree/sidebar-items.js
+++ docs/rustdoc/master/kanidm/tracing_tree/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["EventTag",""]],"fn":[["main_init",""],["operation_id",""],["test_init",""]],"struct":[["TreeMiddleware",""],["TreePreProcessed",""],["TreeSubscriber",""]]});
+initSidebarItems({"enum":[["EventTag",""]],"fn":[["main_init",""],["operation_id",""],["test_init",""],["test_level",""]],"struct":[["TreeMiddleware",""],["TreePreProcessed",""],["TreeSubscriber",""]]});
