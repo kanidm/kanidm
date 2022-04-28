d8ae55986676d2e6137d527b858f1660e1a20dbc
James Hodgkinson <james@terminaloutcomes.com>
2022-04-28 22:45:03 +1000

    trying to fix docs generation

--- build_all_the_docs.sh
+++ build_all_the_docs.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-git config --global pull.ff only
 export CARGO_TARGET_DIR="${TMPDIR}cargo_target"
 DOCS_DIR="/tmp/kanidm_docs"
 
@@ -11,15 +10,18 @@ function build_version() {
     BOOK_VERSION=$1
     echo "Book version: ${BOOK_VERSION}"
     echo "<li><a href=\"/kanidm/${BOOK_VERSION}\">${BOOK_VERSION}</a></li>" >> "${DOCS_DIR}/index.html"
-	git switch -c "${BOOK_VERSION}"
+	git switch -c "${BOOK_VERSION}" || git switch "${BOOK_VERSION}"
 	git pull origin "${BOOK_VERSION}"
-	RUSTFLAGS=-Awarnings cargo doc --quiet --no-deps
-	mdbook build kanidm_book
+	echo "Running mdbook"
+    mdbook build kanidm_book
 	mv ./kanidm_book/book/ "${DOCS_DIR}/${BOOK_VERSION}/"
+    echo "Running cargo doc"
+    cargo doc --no-deps
 	mkdir -p "${DOCS_DIR}/${BOOK_VERSION}/rustdoc/"
 	mv ./target/doc/* "${DOCS_DIR}/${BOOK_VERSION}/rustdoc/"
 }
 
+rm -rf "${DOCS_DIR}"
 mkdir -p "${DOCS_DIR}"
 
 cat > "${DOCS_DIR}/index.html" <<-'EOM'
@@ -56,4 +58,10 @@ EOM
 ls -la "${DOCS_DIR}"
 
 mv "${DOCS_DIR}" ./docs/
+
+# keep jekyll outta things
+touch ./docs/.nojekyll
+
+# link latest to stable
 ln -s "${LATEST}" ./docs/stable
+
