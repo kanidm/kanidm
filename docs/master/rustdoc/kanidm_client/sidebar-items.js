7fdbea3b77072c3fbda9e1f3d7b1c158ae279304
Firstyear <Firstyear@users.noreply.github.com>
2022-04-29 03:36:06 +0000

    deploy: 53f326028573fdbfa985ba48fb86b850b763012b

--- docs/master/rustdoc/kanidm_client/sidebar-items.js
+++ docs/master/rustdoc/kanidm_client/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"constant":[["APPLICATION_JSON",""],["KOPID",""],["KSESSIONID",""]],"enum":[["ClientError",""]],"mod":[["asynchronous",""]],"struct":[["KanidmClientBuilder",""],["StatusCode","An HTTP status code (`status-code` in RFC 7230 et al.)."]]});
+initSidebarItems({"constant":[["APPLICATION_JSON",""],["KOPID",""],["KSESSIONID",""]],"enum":[["ClientError",""]],"struct":[["KanidmClient",""],["KanidmClientBuilder",""],["StatusCode","An HTTP status code (`status-code` in RFC 7230 et al.)."]]});
