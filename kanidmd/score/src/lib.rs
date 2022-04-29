d40f524f8fa400aa801cfa08346da3ebd529c4d1
William Brown <william@blackhats.net.au>
2022-04-29 12:19:35 +1000

    Gosh dang it cloppy

--- kanidmd/score/src/lib.rs
+++ kanidmd/score/src/lib.rs
@@ -8,7 +8,7 @@
 //! the entry point for all client traffic which is then directed to the
 //! various `actors`.
 
-// #![deny(warnings)]
+#![deny(warnings)]
 #![warn(unused_extern_crates)]
 #![deny(clippy::todo)]
 #![deny(clippy::unimplemented)]
