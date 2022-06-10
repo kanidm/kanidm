c5ea14d0cff57419c9d734e45d41673d0d5cd59b
James Hodgkinson <james@terminaloutcomes.com>
2022-06-10 15:07:19 +1000

    adding new favicon and square logo, updating build scripts (#819)

--- kanidmd_web_ui/build_wasm_dev.sh
+++ kanidmd_web_ui/build_wasm_dev.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-wasm-pack build --dev --target web && \
-    touch ./pkg/ANYTHING_HERE_WILL_BE_DELETED_ADD_TO_SRC && \
+wasm-pack build --dev --target web || exit 1
+
+touch ./pkg/ANYTHING_HERE_WILL_BE_DELETED_ADD_TO_SRC && \
+    cp -R ./src/img ./pkg/ && \
     cp ./src/style.css ./pkg/style.css && \
     cp ./src/wasmloader.js ./pkg/wasmloader.js && \
-    cp ./src/favicon.svg ./pkg/favicon.svg && \
     cp -a ./src/external ./pkg/external && \
     rm ./pkg/.gitignore
 
