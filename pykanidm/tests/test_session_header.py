368e62a7c788a04e7e3aa71bd0222842e23f1297
James Hodgkinson <james@terminaloutcomes.com>
2022-06-14 21:50:34 +1000

    mypy pylint and black

--- pykanidm/tests/test_session_header.py
+++ pykanidm/tests/test_session_header.py
@@ -2,6 +2,7 @@
 
 import pytest
 
+import aiohttp.client_exceptions
 from testutils import client
 
 from kanidm import KanidmClient
@@ -16,3 +17,20 @@ def test_session_header(client: KanidmClient) -> None:
     assert client.session_header("testval") == {
         "X-KANIDM-AUTH-SESSION-ID": "testval",
     }
+
+
+@pytest.mark.asyncio
+async def test_session_creator(client: KanidmClient) -> None:
+    """tests the session_header function"""
+
+    client.session = None
+    client.config.uri = "🦀"
+    with pytest.raises(aiohttp.client_exceptions.InvalidURL):
+        await client._call(method="GET", path="/")  # pylint: disable=protected-access
+
+    #  pytest.raises(ValueError):
+    #     client.session_header()
+
+    # assert client.session_header("testval") == {
+    #     "X-KANIDM-AUTH-SESSION-ID": "testval",
+    # }
--- pykanidm/tests/test_types.py
+++ pykanidm/tests/test_types.py
@@ -1,6 +1,9 @@
 """ tests types """
 
-from kanidm.types import AuthInitResponse
+import pytest
+import pydantic.error_wrappers
+
+from kanidm.types import AuthInitResponse, KanidmClientConfig, RadiusGroup
 
 
 def test_auth_init_response() -> None:
@@ -14,3 +17,37 @@ def test_auth_init_response() -> None:
 
     testval = AuthInitResponse.parse_obj(testobj)
     assert testval.sessionid == "crabzrool"
+
+
+def test_radiusgroup_vlan_negative() -> None:
+    """tests RadiusGroup's vlan validator"""
+    with pytest.raises(pydantic.error_wrappers.ValidationError):
+        RadiusGroup(vlan=-1)
+
+
+def test_radiusgroup_vlan_zero() -> None:
+    """tests RadiusGroup's vlan validator"""
+    with pytest.raises(pydantic.error_wrappers.ValidationError):
+        RadiusGroup(vlan=0)
+
+
+def test_radiusgroup_vlan_4096() -> None:
+    """tests RadiusGroup's vlan validator"""
+    assert RadiusGroup(vlan=4096, name="crabzrool")
+
+
+def test_radiusgroup_vlan_no_name() -> None:
+    """tests RadiusGroup's vlan validator"""
+    with pytest.raises(
+        pydantic.error_wrappers.ValidationError, match="name\n.*field required"
+    ):
+        RadiusGroup(
+            vlan=4096,
+        )
+
+
+def test_kanidmconfig_parse_toml() -> None:
+    """tests KanidmClientConfig.parse_toml()"""
+
+    config = KanidmClientConfig()
+    config.parse_toml("uri = 'https://crabzrool.example.com'")
