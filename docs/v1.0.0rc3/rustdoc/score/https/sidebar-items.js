676ddc3e44d200e5b9cbb0c8270db9bafd2b3975
yaleman <yaleman@users.noreply.github.com>
2022-07-03 01:31:28 +0000

    deploy: 65cf0c7f126ec7486b5bb94ecc45782c6c9bf13c

---
+++ docs/v1.0.0rc3/rustdoc/score/https/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["compression_content_type_checker","This is for the tide_compression middleware so that we only compress certain content types."],["create_https_server",""],["generate_integrity_hash",""],["to_tide_response",""]],"struct":[["AppState",""]],"trait":[["RequestExtensions",""]]});
