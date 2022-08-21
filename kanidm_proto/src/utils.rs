232157c377b9339cd1ddbf8541e05d3d12b5ea8b
James Hodgkinson <james@terminaloutcomes.com>
2022-08-22 06:47:34 +1000

    disable last-git-commit in WASM

--- kanidm_proto/src/utils.rs
+++ kanidm_proto/src/utils.rs
@@ -5,8 +5,11 @@ pub fn show_version(name: &str) {
 
 pub fn get_version(name: &str) -> String {
     let version = env!("CARGO_PKG_VERSION");
+    #[cfg(not(target_family = "wasm"))]
     match last_git_commit::LastGitCommit::new().build() {
         Ok(value) => format!("{} {} {}", name, version, value.id().short()),
         Err(_) => format!("{} {}", name, version),
     }
+    #[cfg(target_family = "wasm")]
+    format!("{} {}", name, version)
 }
