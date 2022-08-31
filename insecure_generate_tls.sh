fba90cce9eb0bea0eae3135e89e6c9b24f7509b5
William Brown <william@blackhats.net.au>
2022-08-31 15:44:15 +1000

    fix local tls

--- insecure_generate_tls.sh
+++ insecure_generate_tls.sh
@@ -13,8 +13,11 @@ if [ -z "$KANI_TMP" ]; then
 fi
 
 ALTNAME_FILE="${KANI_TMP}altnames.cnf"
+CANAME_FILE="${KANI_TMP}ca.cnf"
 CACERT="${KANI_TMP}ca.pem"
 CAKEY="${KANI_TMP}cakey.pem"
+CADB="${KANI_TMP}ca.txt"
+CASRL="${KANI_TMP}ca.srl"
 
 KEYFILE="${KANI_TMP}key.pem"
 CERTFILE="${KANI_TMP}cert.pem"
@@ -27,7 +30,78 @@ if [ ! -d "${KANI_TMP}" ]; then
     mkdir -p "${KANI_TMP}"
 fi
 
+cat > "${CANAME_FILE}" << DEVEOF
+[req]
+nsComment = "Certificate Authority"
+distinguished_name  = req_distinguished_name
+req_extensions = v3_ca
+
+[ req_distinguished_name ]
+
+countryName                     = Country Name (2 letter code)
+countryName_default             = AU
+countryName_min                 = 2
+countryName_max                 = 2
+
+stateOrProvinceName             = State or Province Name (full name)
+stateOrProvinceName_default     = Queensland
+
+localityName                    = Locality Name (eg, city)
+localityName_default            = Brisbane
+
+0.organizationName              = Organization Name (eg, company)
+0.organizationName_default      = INSECURE EXAMPLE
+
+organizationalUnitName          = Organizational Unit Name (eg, section)
+organizationalUnitName_default =  kanidm
+
+commonName                      = Common Name (eg, your name or your server\'s hostname)
+commonName_max                  = 64
+commonName_default              = insecure.ca.localhost
+
+[ v3_ca ]
+subjectKeyIdentifier = hash
+basicConstraints = critical, CA:true
+keyUsage = critical, digitalSignature, cRLSign, keyCertSign
+
+DEVEOF
+
 cat > "${ALTNAME_FILE}" << DEVEOF
+
+[ca]
+default_ca = CA_default
+
+[ CA_default ]
+# Directory and file locations.
+dir               = ${KANI_TMP}
+certs             = ${KANI_TMP}
+crl_dir           = ${KANI_TMP}
+new_certs_dir     = ${KANI_TMP}
+database          = ${CADB}
+serial            = ${CASRL}
+
+# The root key and root certificate.
+private_key       = ${CAKEY}
+certificate       = ${CACERT}
+
+# SHA-1 is deprecated, so use SHA-2 instead.
+default_md        = sha256
+
+name_opt          = ca_default
+cert_opt          = ca_default
+default_days      = 3650
+preserve          = no
+policy            = policy_loose
+
+[ policy_loose ]
+countryName             = optional
+stateOrProvinceName     = optional
+localityName            = optional
+organizationName        = optional
+organizationalUnitName  = optional
+commonName              = supplied
+emailAddress            = optional
+
 [req]
 nsComment = "Certificate"
 distinguished_name  = req_distinguished_name
@@ -54,14 +128,15 @@ organizationalUnitName_default =  kanidm
 
 commonName                      = Common Name (eg, your name or your server\'s hostname)
 commonName_max                  = 64
-commonName_default              = localhost
+commonName_default              = ${CERT_HOSTNAME}
 
 [ v3_req ]
-
-# Extensions to add to a certificate request
-
 basicConstraints = CA:FALSE
-keyUsage = nonRepudiation, digitalSignature, keyEncipherment
+nsCertType = server
+nsComment = "Server Certificate"
+subjectKeyIdentifier = hash
+keyUsage = critical, digitalSignature, keyEncipherment
+extendedKeyUsage = serverAuth
 subjectAltName = @alt_names
 
 [alt_names]
@@ -70,39 +145,37 @@ IP.1 = 127.0.0.1
 
 DEVEOF
 
-
+touch ${CADB}
+echo 1000 > ${CASRL}
 
 # Make the ca key
 openssl ecparam -genkey -name prime256v1 -noout -out "${CAKEY}"
 
 # Self sign the CA.
-openssl req -x509 -new -sha256 \
+openssl req -config "${CANAME_FILE}" \
     -key "${CAKEY}" \
+    -new -x509 -days +31 \
+    -sha256 -extensions v3_ca \
     -out "${CACERT}" \
-    -days +31 \
-    -subj "/C=AU/ST=Queensland/L=Brisbane/O=INSECURE/CN=insecure.ca.localhost" -nodes
+    -nodes
 
 # generate the server private key
 openssl ecparam -genkey -name prime256v1 -noout -out "${KEYFILE}"
 
 # generate the certficate signing request
-openssl req -sha256 \
-    -config "${ALTNAME_FILE}" \
-    -new -extensions v3_req \
+openssl req -sha256 -new \
+    -config "${ALTNAME_FILE}" -extensions v3_req \
     -key "${KEYFILE}"\
-    -subj "/C=AU/ST=Queensland/L=Brisbane/O=INSECURE/CN=${CERT_HOSTNAME}" \
     -nodes \
     -out "${CSRFILE}"
 
 # sign the cert
-openssl x509 -req -days 31 \
-    -extfile "${ALTNAME_FILE}" \
-    -CA "${CACERT}" \
-    -CAkey "${CAKEY}" \
-    -CAcreateserial \
+openssl ca -config "${ALTNAME_FILE}" \
+    -extensions v3_req \
+    -days 31 -notext -md sha256 \
     -in "${CSRFILE}" \
-    -out "${CERTFILE}" \
-    -extensions v3_req -sha256
+    -out "${CERTFILE}"
+
 # Create the chain
 cat "${CERTFILE}" "${CACERT}" > "${CHAINFILE}"
 
