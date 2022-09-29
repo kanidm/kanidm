b30c3e3f7d0df54d4bf7e91667a73bd5c878cab9
James Hodgkinson <james@terminaloutcomes.com>
2022-09-30 08:22:28 +1000

    actually adding files I thought I had

---
+++ pykanidm/kanidm/radius/utils.py
@@ -0,0 +1,31 @@
+""" class utils """
+
+from typing import Optional
+import logging
+
+from .. import KanidmClient
+from ..types import RadiusTokenGroup
+
+def check_vlan(
+    acc: int,
+    group: RadiusTokenGroup,
+    kanidm_client: Optional[KanidmClient] = None,
+) -> int:
+    """checks if a vlan is in the config,
+
+    acc is the default vlan
+    """
+    logging.debug("acc=%s", acc)
+    if kanidm_client is None:
+        # kanidm_client = KANIDM_CLIENT
+        raise ValueError("Need to pass this a kanidm_client")
+
+    for radius_group in kanidm_client.config.radius_groups:
+        logging.debug(
+            "Checking vlan group '%s' against user group %s", radius_group.spn, group.spn
+        )
+        if radius_group.spn == group.spn:
+            logging.info("returning new vlan: %s", radius_group.vlan)
+            return radius_group.vlan
+    logging.debug("returning already set vlan: %s", acc)
+    return acc
