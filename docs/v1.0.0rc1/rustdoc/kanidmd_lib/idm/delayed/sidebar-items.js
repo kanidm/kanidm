99098ef82aaa06e0457e30122ac57a9947765515
yaleman <yaleman@users.noreply.github.com>
2022-11-13 04:27:06 +0000

    deploy: 06c9e087cbdd74c9116326f369c4e4633cfb938d

--- docs/v1.0.0rc1/rustdoc/kanidmd_lib/idm/delayed/sidebar-items.js
+++ docs/v1.0.0rc1/rustdoc/kanidmd_lib/idm/delayed/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":[["DelayedAction",""]],"struct":[["AuthSessionRecord",""],["BackupCodeRemoval",""],["Oauth2ConsentGrant",""],["PasswordUpgrade",""],["UnixPasswordUpgrade",""],["WebauthnCounterIncrement",""]]};
+window.SIDEBAR_ITEMS = {"enum":[["DelayedAction",""]],"struct":[["AuthSessionRecord",""],["BackupCodeRemoval",""],["Oauth2ConsentGrant",""],["Oauth2SessionRecord",""],["PasswordUpgrade",""],["UnixPasswordUpgrade",""],["WebauthnCounterIncrement",""]]};
