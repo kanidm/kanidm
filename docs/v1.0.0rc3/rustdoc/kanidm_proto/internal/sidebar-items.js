ca7f841343cd6fafab1b9a0d0ed3b80950367289
yaleman <yaleman@users.noreply.github.com>
2022-11-23 13:53:51 +0000

    deploy: 883aa5d5cbec4c0ee08a8e73a5feedeb76980e34

---
+++ docs/v1.0.0rc3/rustdoc/kanidm_proto/internal/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":[["AppLink","This is a description of a linked or connected application for a user. This is used in the UI to render applications on the dashboard for a user to access."]]};
