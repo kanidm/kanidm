bee6cc4e00d8f52fa7a29c3063e2f3451be6b3ce
yaleman <yaleman@users.noreply.github.com>
2022-12-15 21:58:21 +0000

    deploy: 6f3f1669042f0d750a95d5bdd1fe3f1005e8fbbe

--- docs/v1.1.0-alpha/rustdoc/implementors/core/ops/drop/trait.Drop.js
+++ docs/v1.1.0-alpha/rustdoc/implementors/core/ops/drop/trait.Drop.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"kanidmd_core":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.65.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"kanidmd_core/struct.CoreHandle.html\" title=\"struct kanidmd_core::CoreHandle\">CoreHandle</a>"]]
+"kanidmd_core":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.66.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"kanidmd_core/struct.CoreHandle.html\" title=\"struct kanidmd_core::CoreHandle\">CoreHandle</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
