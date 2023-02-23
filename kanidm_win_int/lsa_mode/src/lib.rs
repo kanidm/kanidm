f1a21e1c8aa1dc395ed877160ea3b541467a547f
Matthew Wilks <water261@outlook.com.au>
2023-02-24 09:20:23 +1000

    Add libraries + init funcs

---
+++ kanidm_win_int/lsa_mode/src/lib.rs
@@ -0,0 +1,12 @@
+use windows::Win32::{Foundation::NTSTATUS, Security::Authentication::Identity::*};
+
+#[no_mangle]
+#[allow(non_snake_case, unused_variables)]
+pub extern "system" fn SpLsaModeInitialize(
+    lsaversion: u32,
+    packageversion: *mut u32,
+    pptables: *mut *mut SECPKG_FUNCTION_TABLE,
+    pctables: *mut u32,
+) -> NTSTATUS {
+    NTSTATUS(0x0)
+}
