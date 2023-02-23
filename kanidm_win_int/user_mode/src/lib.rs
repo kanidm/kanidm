8bc7d3808e131165f6d1b395cdbfbeef3e50d6bd
Matthew Wilks <water261@outlook.com.au>
2023-02-24 09:27:13 +1000

    Use correct func table for UserMode Init

--- kanidm_win_int/user_mode/src/lib.rs
+++ kanidm_win_int/user_mode/src/lib.rs
@@ -5,7 +5,7 @@ use windows::Win32::{Foundation::NTSTATUS, Security::Authentication::Identity::*
 pub extern "system" fn SpUserModeInitialize(
     lsaversion: u32,
     packageversion: *mut u32,
-    pptables: *mut *mut SECPKG_FUNCTION_TABLE,
+    pptables: *mut *mut SECPKG_USER_FUNCTION_TABLE,
     pctables: *mut u32,
 ) -> NTSTATUS {
     NTSTATUS(0x0)
