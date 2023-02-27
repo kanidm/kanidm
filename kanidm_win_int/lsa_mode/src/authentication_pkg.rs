100be1bf8242d03206fa76fdd86864ac14534028
Matthew Wilks <water261@outlook.com.au>
2023-02-27 20:08:25 +1000

    Implement some of the needed functions

---
+++ kanidm_win_int/lsa_mode/src/authentication_pkg.rs
@@ -0,0 +1,108 @@
+use std::ffi::c_void;
+use windows::Win32::{Foundation::*, Security::Authentication::Identity::*, System::Kernel::*};
+
+pub extern "system" fn ap_initialise_pkg(
+    auth_pkg_id: u32,
+    lsa_dispatch_table: *const LSA_DISPATCH_TABLE,
+    db: *const STRING,
+    confidentiality: *const STRING,
+    auth_pkg_name: *mut *mut STRING,
+) -> NTSTATUS {
+    NTSTATUS(0x0)
+}
+
+pub extern "system" fn ap_logon_user(
+    client_req: *const *const c_void,
+    logon_type: SECURITY_LOGON_TYPE,
+    auth_info: *const c_void,
+    client_auth_base: *const c_void,
+    auth_info_length: u32,
+    profile_buffer: *mut *mut c_void,
+    profile_buffer_length: *mut u32,
+    logon_id: *mut LUID,
+    substatus: *mut i32,
+    token_info_type: *mut LSA_TOKEN_INFORMATION_TYPE,
+    token_info: *mut *mut c_void,
+    account_name: *mut *mut UNICODE_STRING,
+    auth_authority: *mut *mut UNICODE_STRING,
+) -> NTSTATUS {
+    NTSTATUS(0x0)
+}
+
+pub extern "system" fn ap_call_package(
+    client_req: *const *const c_void,
+    protocol_submit_buffer: *const c_void,
+    client_buffer_base: *const c_void,
+    submit_buffer_length: u32,
+    protocol_return_buffer: *mut *mut c_void,
+    return_buffer_length: *mut u32,
+    protcol_status: *mut i32,
+) -> NTSTATUS {
+    NTSTATUS(0x0)
+}
+
+pub extern "system" fn ap_call_package_untrusted(
+    client_req: *const *const c_void,
+    protocol_submit_buffer: *const c_void,
+    client_buffer_base: *const c_void,
+    submit_buffer_length: u32,
+    protocol_return_buffer: *mut *mut c_void,
+    return_buffer_length: *mut u32,
+    protcol_status: *mut i32,
+) -> NTSTATUS {
+    NTSTATUS(0x0)
+}
+
+pub extern "system" fn ap_call_package_passthrough(
+    client_req: *const *const c_void,
+    protocol_submit_buffer: *const c_void,
+    client_buffer_base: *const c_void,
+    submit_buffer_length: u32,
+    protocol_return_buffer: *mut *mut c_void,
+    return_buffer_length: *mut u32,
+    protcol_status: *mut i32,
+) -> NTSTATUS {
+    NTSTATUS(0x0)
+}
+
+pub extern "system" fn ap_logon_terminated(logon_id: *const LUID) {}
+
+pub extern "system" fn ap_logon_user_ex(
+    client_req: *const *const c_void,
+    logon_type: SECURITY_LOGON_TYPE,
+    auth_info: *const c_void,
+    client_auth_base: *const c_void,
+    auth_info_length: u32,
+    profile_buffer: *mut *mut c_void,
+    profile_buffer_length: *mut u32,
+    logon_id: *mut LUID,
+    substatus: *mut i32,
+    token_info_type: *mut LSA_TOKEN_INFORMATION_TYPE,
+    token_info: *mut *mut c_void,
+    account_name: *mut *mut UNICODE_STRING,
+    auth_authority: *mut *mut UNICODE_STRING,
+    machinename: *mut *mut UNICODE_STRING,
+) -> NTSTATUS {
+    NTSTATUS(0x0)
+}
+
+pub extern "system" fn ap_logon_user_ex2(
+    client_req: *const *const c_void,
+    logon_type: SECURITY_LOGON_TYPE,
+    protocol_submit_buffer: *const c_void,
+    client_buffer_base: *const c_void,
+    submit_buffer_size: u32,
+    profile_buffer: *mut *mut c_void,
+    profile_buffer_size: *mut u32,
+    logon_id: *mut LUID,
+    substatus: *mut i32,
+    token_info_type: *mut LSA_TOKEN_INFORMATION_TYPE,
+    token_info: *mut *mut c_void,
+    account_name: *mut *mut UNICODE_STRING,
+    auth_authority: *mut *mut UNICODE_STRING,
+    machinename: *mut *mut UNICODE_STRING,
+    primary_credentials: *mut SECPKG_PRIMARY_CRED,
+    supplemental_credentials: *mut *mut SECPKG_SUPPLEMENTAL_CRED_ARRAY,
+) -> NTSTATUS {
+    NTSTATUS(0x0)
+}
--- kanidm_win_int/lsa_mode/src/lib.rs
+++ kanidm_win_int/lsa_mode/src/lib.rs
@@ -1,12 +1,27 @@
-use windows::Win32::{Foundation::NTSTATUS, Security::Authentication::Identity::*};
+use windows::Win32::{Foundation::*, Security::Authentication::Identity::*};
+use authentication_pkg as auth_pkg;
+
+mod authentication_pkg;
 
 #[no_mangle]
-#[allow(non_snake_case, unused_variables)]
+#[allow(non_snake_case)]
 pub extern "system" fn SpLsaModeInitialize(
     lsaversion: u32,
     packageversion: *mut u32,
     pptables: *mut *mut SECPKG_FUNCTION_TABLE,
     pctables: *mut u32,
 ) -> NTSTATUS {
+    unsafe {
+        let mut tbl_ref = *(*pptables);
+        tbl_ref.InitializePackage = Some(auth_pkg::ap_initialise_pkg);
+        tbl_ref.LogonUserA = Some(auth_pkg::ap_logon_user);
+        tbl_ref.CallPackage = Some(auth_pkg::ap_call_package);
+        tbl_ref.LogonTerminated = Some(auth_pkg::ap_logon_terminated);
+        tbl_ref.CallPackageUntrusted = Some(auth_pkg::ap_call_package_untrusted);
+        tbl_ref.CallPackagePassthrough = Some(auth_pkg::ap_call_package_passthrough);
+        tbl_ref.LogonUserExA = Some(auth_pkg::ap_logon_user_ex);
+        tbl_ref.LogonUserEx2 = Some(auth_pkg::ap_logon_user_ex2);
+    }
+
     NTSTATUS(0x0)
 }
