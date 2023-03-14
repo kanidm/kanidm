c924515e2be3562abda08bed9c92f3b2682b4439
MinhPhan8803 <minhnp2@illinois.edu>
2023-03-14 15:22:47 -0500

    Actually handle debug tracing

--- unix_integration/src/daemon.rs
+++ unix_integration/src/daemon.rs
@@ -427,7 +427,7 @@ async fn main() -> ExitCode {
         .map_sender(|sender| sender.or_stderr())
         .build_on(|subscriber| subscriber
             .with(EnvFilter::try_from_default_env()
-                .or_else(|_| EnvFilter::try_new("info"))
+                .or_else(|_| EnvFilter::try_new("debug"))
                 .expect("Failed to init envfilter")
             )
         )
