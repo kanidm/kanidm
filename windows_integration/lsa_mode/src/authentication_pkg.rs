e5e4fefa987fed9ccff2a94bb50aa9eec3c6e313
Matthew Wilks <water261@outlook.com.au>
2023-03-27 18:58:45 +1000

    Move state inside a struct

--- windows_integration/lsa_mode/src/authentication_pkg.rs
+++ windows_integration/lsa_mode/src/authentication_pkg.rs
@@ -1,17 +1,94 @@
 use std::ffi::c_void;
+use std::sync::{Arc, Mutex};
+use tracing::{error, warn};
 use windows::core::PSTR;
 use windows::Win32::{Foundation::*, Security::Authentication::Identity::*, System::Kernel::*};
 
 struct AuthPkg {
     /// The authentication package id assigned by the local security authority
-    api: Option<u32>,
+    package_id: Option<Arc<u32>>,
     /// The lsa dispatch table
-    ldt: Option<*const LSA_DISPATCH_TABLE>,
+    dispatch_table: Option<Arc<&'static LSA_DISPATCH_TABLE>>,
+}
+
+impl AuthPkg {
+    pub fn init_pkg(
+        self: &mut Self,
+        auth_pkg_id: u32,
+        lsa_dispatch_table: *const LSA_DISPATCH_TABLE,
+        auth_pkg_name: *mut *mut STRING,
+    ) -> NTSTATUS {
+        // Check if we've already setup a dispatch table
+        if self.dispatch_table.is_some() {
+            error!("dispatch table already exists");
+
+            // Error out as these already existing imply that this function has already been called
+            // Hence the Authentication package is trying to be initialised more than once
+            return STATUS_UNSUCCESSFUL;
+        }
+
+        // Because const ptrs has issues with static mut, we just want a ref instead
+        let lsa_dt = unsafe {
+            match lsa_dispatch_table.as_ref() {
+                Some(dt) => dt,
+                None => {
+                    error!("failed to get lsa dispatch table as ref");
+                    return STATUS_UNSUCCESSFUL;
+                }
+            }
+        };
+
+        let am_lsa_dt = Arc::new(lsa_dt);
+        self.dispatch_table = Some(am_lsa_dt);
+
+        if self.package_id.is_some() {
+            error!("package id already exists");
+            return STATUS_UNSUCCESSFUL;
+        }
+
+        self.package_id = Some(Arc::new(auth_pkg_id));
+
+        let mut pkg_name = env!("CARGO_PKG_NAME").to_owned();
+        let pkg_name_len = match u16::try_from(pkg_name.len()) {
+            Ok(len) => len,
+            Err(_) => return STATUS_UNSUCCESSFUL,
+        };
+        let ap_name = STRING {
+            Length: pkg_name_len,
+            MaximumLength: pkg_name_len,
+            Buffer: PSTR(pkg_name.as_mut_ptr()),
+        };
+
+        unsafe {
+            **auth_pkg_name = ap_name;
+        }
+
+        STATUS_SUCCESS
+    }
+
+    pub fn logon_user(
+        self: &mut Self,
+        client_req: *const *const c_void,
+        logon_type: SECURITY_LOGON_TYPE,
+        auth_info: *const c_void,
+        client_auth_base: *const c_void,
+        auth_info_length: u32,
+        profile_buffer: *mut *mut c_void,
+        profile_buffer_length: *mut u32,
+        logon_id: *mut LUID,
+        substatus: *mut i32,
+        token_info_type: *mut LSA_TOKEN_INFORMATION_TYPE,
+        token_info: *mut *mut c_void,
+        account_name: *mut *mut UNICODE_STRING,
+        auth_authority: *mut *mut UNICODE_STRING,
+    ) -> NTSTATUS {
+        STATUS_SUCCESS
+    }
 }
 
 static mut AUTH_PKG: AuthPkg = AuthPkg {
-    api: None,
-    ldt: None,
+    package_id: None,
+    dispatch_table: None,
 };
 
 pub extern "system" fn ap_initialise_pkg(
@@ -21,25 +98,7 @@ pub extern "system" fn ap_initialise_pkg(
     _: *const STRING,
     auth_pkg_name: *mut *mut STRING,
 ) -> NTSTATUS {
-    let mut pkg_name = env!("CARGO_PKG_NAME").to_owned();
-    let pkg_name_len = match u16::try_from(pkg_name.len()) {
-        Ok(len) => len,
-        Err(_) => return STATUS_UNSUCCESSFUL,
-    };
-
-    let ap_name = STRING {
-        Length: pkg_name_len,
-        MaximumLength: pkg_name_len,
-        Buffer: PSTR(pkg_name.as_mut_ptr()),
-    };
-
-    unsafe {
-        **auth_pkg_name = ap_name;
-        AUTH_PKG.ldt = Some(lsa_dispatch_table);
-        AUTH_PKG.api = Some(auth_pkg_id);
-    }
-
-    STATUS_SUCCESS
+    unsafe { AUTH_PKG.init_pkg(auth_pkg_id, lsa_dispatch_table, auth_pkg_name) }
 }
 
 pub extern "system" fn ap_logon_user(
@@ -57,7 +116,23 @@ pub extern "system" fn ap_logon_user(
     account_name: *mut *mut UNICODE_STRING,
     auth_authority: *mut *mut UNICODE_STRING,
 ) -> NTSTATUS {
-    STATUS_SUCCESS
+    unsafe {
+        AUTH_PKG.logon_user(
+            client_req,
+            logon_type,
+            auth_info,
+            client_auth_base,
+            auth_info_length,
+            profile_buffer,
+            profile_buffer_length,
+            logon_id,
+            substatus,
+            token_info_type,
+            token_info,
+            account_name,
+            auth_authority,
+        )
+    }
 }
 
 pub extern "system" fn ap_call_package(
