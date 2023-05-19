795531430c2d9e976244e3a70bbaacaceb51d0a7
Matthew Wilks <water261@outlook.com.au>
2023-05-19 18:33:18 +1000

    Fix LogonId property naming

--- windows_integration/security_package/src/structs.rs
+++ windows_integration/security_package/src/structs.rs
@@ -13,12 +13,12 @@ pub struct ProfileBuffer {
 
 #[derive(Clone, Copy, Hash, PartialEq, Eq)]
 pub struct LogonId {
-    pub LowPart: u32,
-    pub HighPart: i32,
+    pub low_part: u32,
+    pub high_part: i32,
 }
 
 impl From<LUID> for LogonId {
     fn from(luid: LUID) -> Self {
-        LogonId { LowPart: luid.LowPart, HighPart: luid.HighPart }
+        LogonId { low_part: luid.LowPart, high_part: luid.HighPart }
     }
 }
