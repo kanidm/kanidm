ffd06ae350b4d1ae4ef0dce206f7cb6883bdcb33
Matthew Wilks <water261@outlook.com.au>
2023-05-24 08:45:40 +1000

    Move auth pkg protocol to root lib.rs

--- windows_integration/public/src/lib.rs
+++ windows_integration/public/src/lib.rs
@@ -1,1 +1,20 @@
-pub mod secpkg;
+pub enum AccountType {
+	Person,
+	Service,
+}
+
+// Authentication Package Protocol
+pub enum AuthPkgRequest {
+	AuthenticateAccount(AuthenticateAccountRequest),
+}
+
+pub enum AuthPkgResponse {
+	AuthenticateAccount(AuthenticateAccountResponse),
+	Error(AuthPkgError),
+}
+
+pub enum AuthPkgError {}
+
+pub struct AuthenticateAccountRequest {}
+
+pub struct AuthenticateAccountResponse {}
--- windows_integration/public/src/secpkg/ap_proto/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod v1;
-
-pub enum AuthPkgRequest {
-	V1(v1::AuthPkgRequest),
-}
-
-pub enum AuthPkgResponse {
-	V1(v1::AuthPkgResponse),
-}
