8981f285f3cd150fafb0aaa29c8f2b94dba8974d
Matthew Wilks <water261@outlook.com.au>
2023-05-24 12:37:41 +1000

    Format public lib

--- windows_integration/public/src/lib.rs
+++ windows_integration/public/src/lib.rs
@@ -1,31 +1,31 @@
 use kanidm_proto::v1::UnixUserToken;
 
 pub enum AccountType {
-	Person,
-	Service,
+    Person,
+    Service,
 }
 
 // Authentication Package Protocol
 pub enum AuthPkgRequest {
-	AuthenticateAccount(AuthenticateAccountRequest),
+    AuthenticateAccount(AuthenticateAccountRequest),
 }
 
 pub enum AuthPkgResponse {
-	AuthenticateAccount(AuthenticateAccountResponse),
-	Error(AuthPkgError),
+    AuthenticateAccount(AuthenticateAccountResponse),
+    Error(AuthPkgError),
 }
 
 pub enum AuthPkgError {
-	AuthenticationFailed,
+    AuthenticationFailed,
 }
 
 pub struct AuthenticateAccountRequest {
-	pub account_type: AccountType,
-	pub id: String,
-	pub password: String,
+    pub account_type: AccountType,
+    pub id: String,
+    pub password: String,
 }
 
 pub struct AuthenticateAccountResponse {
-	pub status: Result<(), AuthPkgError>,
-	pub token: Option<UnixUserToken>,
+    pub status: Result<(), AuthPkgError>,
+    pub token: Option<UnixUserToken>,
 }
