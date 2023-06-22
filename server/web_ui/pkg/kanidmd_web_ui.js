78c274d1591f3e5125ea7d1596ebbf372f773e04
William Brown <william@blackhats.net.au>
2023-06-22 18:01:46 +1000

    Add oauth2 sort

--- server/web_ui/pkg/kanidmd_web_ui.js
+++ server/web_ui/pkg/kanidmd_web_ui.js
@@ -1159,15 +1159,15 @@ function __wbg_get_imports() {
         const ret = wasm.memory;
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper4682 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper4697 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 1101, __wbg_adapter_48);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper5483 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper5498 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 1400, __wbg_adapter_51);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper5490 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper5505 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 1404, __wbg_adapter_54);
         return addHeapObject(ret);
     };
