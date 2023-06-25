bfdd942922231646885fe0c5ef4514b2fa7ad7f4
James Hodgkinson <james@terminaloutcomes.com>
2023-06-25 22:12:01 +1000

    putting non-rust things in static/

--- server/web_ui/build_wasm.sh
+++ server/web_ui/build_wasm.sh
@@ -26,10 +26,7 @@ fi
 wasm-pack build ${BUILD_FLAGS} --target web || exit 1
 
 touch ./pkg/ANYTHING_HERE_WILL_BE_DELETED_ADD_TO_SRC && \
-    rsync --delete-after -r --copy-links -v ./src/img/ ./pkg/img/ && \
-    rsync --delete-after -r --copy-links -v ./src/external/ ./pkg/external/ && \
+    rsync --delete-after -r --copy-links -v ./static/* ./pkg/ && \
     cp ../../README.md ./pkg/
     cp ../../LICENSE.md ./pkg/
-    cp ./src/style.css ./pkg/style.css && \
-    cp ./src/wasmloader.js ./pkg/wasmloader.js && \
     rm ./pkg/.gitignore
