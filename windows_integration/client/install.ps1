0fefa8fe5d886de2ab100a8d62833a975c0a6dc8
Matthew Wilks <water261@outlook.com.au>
2023-07-04 16:03:06 +1000

    Make path of dll and cfg customisable

--- windows_integration/client/install.ps1
+++ windows_integration/client/install.ps1
@@ -1,12 +1,20 @@
-$kani_dir = "$env:ProgramFiles\kanidm"
+$kani_dir = if ($env:KanidmInstallDirectory -eq $null) { "$env:ProgramFiles\kanidm" } else { $env:KanidmInstallDirectory }
+$kani_dll_name = if ($env:KanidmClientDll -eq $null) { "kanidm_windows_client" } else { $env:KanidmClientDll }
+$kani_dll = if ($env:KanidmClientDllPath -eq $null) { ".\$kani_dll_name.dll" } else { $env:KanidmClientDllPath }
+$kani_cfg = if ($env:KanidmConfigPath -eq $null) { ".\authlib_client.toml" } else { $env:KanidmConfigPath }
 
 if (-not ([Security.Principal.WindowsIdentity]::GetCurrent().Groups -contains 'S-1-5-32-544')) {
 	Write-Output "This script must be run as an administrator"
 	Return
 }
 
-if (-not (Test-Path -Path .\kanidm_windows_client.dll) -or -not (Test-Path -Path .\authlib_client.toml)) {
-	Write-Output "Cannot find the authlib config or dll in the present directory"
+if (-not (Test-Path -Path $kani_dll)) {
+	Write-Output "Cannot find the kanidm config at the specified path"
+	Return
+}
+
+if (-not (Test-Path -Path $kani_cfg)) {
+	Write-Output "Cannot find the kanidm client dll at the specified path"
 	Return
 }
 
@@ -14,11 +22,11 @@ if (-not (Test-Path -Path .\kanidm_windows_client.dll) -or -not (Test-Path -Path
 New-Item -Path $kani_dir -ItemType Directory -Verbose
 
 # Install kanidm authlib & config
-Copy-Item .\kanidm_windows_client.dll -Destination $kani_dir
-Copy-Item .\authlib_client.toml -Destination $kani_dir
+Copy-Item $kani_dll -Destination $kani_dir
+Copy-Item $kani_cfg -Destination $kani_dir
 
 # Register DLL
-Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Lsa" -Name "Security Packages" -Value "C:\Program Files\kanidm\kanidm_windows_client"
+Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Lsa" -Name "Security Packages" -Value "$kani_dir\$kani_dll_name"
 
 # Registry program path
 if (-not (Test-Path -Path "HKLM:\Software\kandim")) {
