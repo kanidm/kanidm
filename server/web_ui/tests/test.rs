576df117daa496415041af4d4b9ccbb995c0d7ed
James Hodgkinson <james@terminaloutcomes.com>
2023-07-08 11:01:39 +1000

    initial testing, doesn't work, stashing things

--- server/web_ui/tests/test.rs
+++ server/web_ui/tests/test.rs
@@ -1,12 +1,15 @@
-/// Test harnesses for WASM things.
-///
-/// Here be crabs with troubling pasts.
-///
-/// Run this on a mac with Safari using the following command:
-///
-/// ```shell
-/// wasm-pack test --safari
-/// ```
+//! Test harnesses for WASM things.
+//!
+//! Here be crabs with troubling pasts.
+//!
+//! Run this on a mac with Safari using the following command:
+//!
+//! ```shell
+//! wasm-pack test --chrome --headless
+//!```
+//!
+
+use headless_chrome::Browser;
 use wasm_bindgen_test::*;
 
 wasm_bindgen_test_configure!(run_in_browser);
@@ -15,3 +18,18 @@ wasm_bindgen_test_configure!(run_in_browser);
 fn pass() {
     assert_eq!(1, 1);
 }
+
+#[kanidmd_testkit::test]
+async fn test_ui_loads(rsclient: kanidm_client::KanidmClient)  {
+    println!("rsclient: {}", rsclient.get_url());
+
+    let browser = Browser::default().unwrap();
+    let tab = browser.new_tab().unwrap();
+
+    tab.navigate_to(rsclient.get_url()).unwrap();
+
+    tab.wait_for_element("input#username").unwrap().click().unwrap();
+    // Type in a query and press `Enter`
+    tab.type_str("testuser").unwrap().press_key("Enter").unwrap();
+}
+
