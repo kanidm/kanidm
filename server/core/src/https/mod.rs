e286c4e526a7c85e9cdbc61bf4f578851ce3863b
James Hodgkinson <james@terminaloutcomes.com>
2023-08-14 18:00:12 +1000

    fixing order of things

--- server/core/src/https/mod.rs
+++ server/core/src/https/mod.rs
@@ -252,19 +252,19 @@ pub async fn create_https_server(
         .layer(from_fn(middleware::version_middleware))
         .layer(from_fn(
             middleware::hsts_header::strict_transport_security_layer,
-        ))
-        .layer(from_fn(middleware::kopid_middleware));
+        ));
 
     // layer which checks the responses have a content-type of JSON when we're in debug mode
     #[cfg(debug_assertions)]
     let app = app.layer(from_fn(middleware::are_we_json_yet));
 
     let app = app
+        .layer(trace_layer)
         // This must be the LAST middleware.
         // This is because the last middleware here is the first to be entered and the last
         // to be exited, and this middleware sets up ids' and other bits for for logging
         // coherence to be maintained.
-        .layer(trace_layer)
+        .layer(from_fn(middleware::kopid_middleware))
         .with_state(state)
         // the connect_info bit here lets us pick up the remote address of the client
         .into_make_service_with_connect_info::<SocketAddr>();
