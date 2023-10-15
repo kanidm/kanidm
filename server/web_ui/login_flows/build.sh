f260d2b5ecbfc5a9e1016ff41d408820d7b3848b
James Hodgkinson <james@terminaloutcomes.com>
2023-10-15 21:40:46 +1000

    checkpoint

---
+++ server/web_ui/login_flows/build.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+set -e
+# This builds the assets for the Admin UI, defaulting to a release build.
+
+if [ ! -f build_admin.sh ]; then
+    echo "Please run from the right directory. (server/web_ui/admin)"
+    exit 1
+fi
+
+if [ -z "${BUILD_FLAGS}" ]; then
+    BUILD_FLAGS="--release"
+    # DURING DEV
+    # BUILD_FLAGS="--dev"
+fi
+
+if [ -z "$(which rsync)" ]; then
+    echo "Cannot find rsync which is needed to move things around, quitting!"
+    exit 1
+fi
+
+if [ -z "$(which wasm-pack)" ]; then
+    echo "Cannot find wasm-pack which is needed to build the UI, quitting!"
+    exit 1
+fi
+
+mkdir -p ./pkg
+
+if [ "$(find ./pkg/ -name 'kanidmd*' | wc -l)" -gt 0 ]; then
+    echo "Cleaning up WASM files before build..."
+    rm pkg/kanidmd*
+fi
+# we can disable this since we want it to expand
+# shellcheck disable=SC2086
+wasm-pack build ${BUILD_FLAGS} --no-typescript --target web --mode no-install --no-pack
+
+echo "######################"
+echo "Moving files around..."
+echo "######################"
+touch ./pkg/ANYTHING_HERE_WILL_BE_DELETED_ADD_TO_SRC && \
+    rsync --delete-after -r --copy-links -v ./static/* ./pkg/ && \
+    cp ../../../README.md ./pkg/
+    cp ../../../LICENSE.md ./pkg/
+    rm ./pkg/.gitignore
+
+echo "######################"
+echo "Moving files up into the webui pkg dir..."
+echo "######################"
+rsync -av pkg/external/* ../pkg/external/
+find ../pkg/ -type f -name 'kanidmd_web_ui_login_flows*' -exec rm {} \;
+rsync -av pkg/kanidmd_web_ui_login_flows* ../pkg/
+
+echo "######################"
+echo "        Done!"
+echo "######################"
