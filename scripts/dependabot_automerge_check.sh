eead47aec822695f2b4322afc2e660f3239f6d19
James Hodgkinson <james@terminaloutcomes.com>
2023-10-16 15:15:53 +1000

    Fixing dependabot and its mistakes (#2232)
    * updating to utoipa 4.0.0
    * hi dependabot

--- scripts/dependabot_automerge_check.sh
+++ scripts/dependabot_automerge_check.sh
@@ -7,20 +7,27 @@ fi
 
 PR_URL="$1"
 
-# are we good?
-CONCLUSIONS="$(gh pr status --json statusCheckRollup | jq '.currentBranch | .[] | .[] | select(.conclusion != "SUCCESS") | select(.conclusion != "NEUTRAL")| {status: .status, workfFlowName: .workFlowName, conclusion: .conclusion}')"
-# check approval
-APPROVED="$(gh pr status --json reviewDecision -q .currentBranch.reviewDecision)"
-
 # sets the upstream metadata for `gh pr status`
 gh pr checkout "$PR_URL"
-if [ "${APPROVED}" != "APPROVED" ]; then
-    echo "PR not approved!"
-    exit 1
-fi
+
+
+# are we good?
+echo "Checking Conclusions"
+CONCLUSIONS="$(gh pr status --json statusCheckRollup | jq '.currentBranch | .[] | .[] | select(.conclusion != "SUCCESS") | select(.conclusion != "NEUTRAL")| {status: .status, workfFlowName: .workFlowName, conclusion: .conclusion}')"
+
+
 
 if [ "$(echo "${CONCLUSIONS}" | wc -l)" -eq 0 ]; then
     gh pr review --approve "$PR_URL"
 else
     echo "Already running or failed: ${CONCLUSIONS}";
 fi
+
+if [ "${APPROVED}" != "APPROVED" ]; then
+    echo "PR isn't approved!"
+    exit 0
+fi
+
+# check approval
+echo "Checking if already approved...."
+APPROVED="$(gh pr status --json reviewDecision -q .currentBranch.reviewDecision)"
