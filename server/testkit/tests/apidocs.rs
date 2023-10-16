ead838dcc8fb61b815439f8dc1391d954d2f3736
James Hodgkinson <james@terminaloutcomes.com>
2023-10-16 14:09:56 +1100

    adding test

---
+++ server/testkit/tests/apidocs.rs
@@ -0,0 +1,22 @@
+use serde::{Deserialize, Serialize};
+use tracing::info;
+
+#[kanidmd_testkit::test]
+async fn check_that_the_swagger_api_loads(rsclient: kanidm_client::KanidmClient) {
+    #[derive(Serialize, Deserialize, Debug)]
+    struct OpenAPIResponse {
+        pub openapi: String,
+    }
+
+    rsclient.set_token("".into()).await;
+    info!("Running test: check_that_the_swagger_api_loads");
+    let url = rsclient.make_url("/docs/v1/openapi.json");
+    let foo: OpenAPIResponse = reqwest::get(url)
+        .await
+        .expect("Failed to get openapi.json")
+        .json()
+        .await
+        .unwrap();
+    assert!(foo.openapi != "1.2.3");
+    println!("Hello world");
+}
