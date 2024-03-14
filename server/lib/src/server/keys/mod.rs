24f9b60e38081f0010934de2f52bac4875e81eaf
William Brown <william@blackhats.net.au>
2024-03-14 17:20:14 +1000

    Okay, designs in my head worked out some more.

--- server/lib/src/server/keys/mod.rs
+++ server/lib/src/server/keys/mod.rs
@@ -3,6 +3,8 @@ mod internal;
 // mod object;
 mod provider;
 
+pub type KeyId = Vec<u8>;
+
 // pub(crate) use self::object::KeyObjects;
 pub(crate) use self::provider::{
     KeyProvider, KeyProviders, KeyProvidersReadTransaction, KeyProvidersTransaction,
