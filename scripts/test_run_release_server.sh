92506cef0c0374618308db5fa5b03eae36876a4c
James Hodgkinson <james@terminaloutcomes.com>
2024-05-07 17:52:14 +1000

    blep

--- scripts/test_run_release_server.sh
+++ scripts/test_run_release_server.sh
@@ -81,11 +81,11 @@ export KANIDM_CA_PATH
 
 echo "Attempting to log out of idm_admin@localhost"
 cd "$(dirname "$(cargo locate-project --workspace | jq -r .root)")" || exit 1
-cargo run --bin kanidm logout -D idm_admin@localhost || ( killall kanidmd; exit 1 )
+cargo run --bin kanidm logout -D idm_admin@localhost
 
 echo "Running the OpenAPI schema checks"
 
-bash -c ./scripts/openapi_tests/check_openapi_spec.sh || killall kanidmd
+bash -c ./scripts/openapi_tests/check_openapi_spec.sh
 
 echo "Waiting ${WAIT_TIMER} seconds and terminating Kanidmd"
 sleep "${WAIT_TIMER}"
