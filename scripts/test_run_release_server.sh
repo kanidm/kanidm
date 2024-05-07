41389b42c65ab91a850ea8977a2efe1e3e7a40cf
James Hodgkinson <james@terminaloutcomes.com>
2024-05-08 08:50:28 +1000

    moar logs

--- scripts/test_run_release_server.sh
+++ scripts/test_run_release_server.sh
@@ -81,7 +81,7 @@ export KANIDM_CA_PATH
 
 echo "Attempting to log out of idm_admin@localhost"
 cd "$(dirname "$(cargo locate-project --workspace | jq -r .root)")" || exit 1
-cargo run --bin kanidm logout -D idm_admin@localhost
+RUST_LOG=DEBUG cargo run --bin kanidm logout -D idm_admin@localhost
 
 echo "Last result: $?"
 
