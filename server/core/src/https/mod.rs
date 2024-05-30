662ff2c78e701421791e9d30e16691b6dc45f47a
James Hodgkinson <james@terminaloutcomes.com>
2024-05-30 12:01:09 +1000

    Update server/core/src/https/mod.rs

--- server/core/src/https/mod.rs
+++ server/core/src/https/mod.rs
@@ -593,7 +593,6 @@ pub(crate) async fn handle_conn(
 
             debug!(?client_conn_info);
 
-            // let svc = tower::MakeService::make_service(&mut app, client_conn_info);
             let svc = tower::MakeService::<ClientConnInfo, hyper::Request<Body>>::make_service(
                 &mut app,
                 client_conn_info,
