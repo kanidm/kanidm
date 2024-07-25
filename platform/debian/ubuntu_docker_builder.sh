38b0a6f8af032d18462f7434b90287d0ccc18a7a
James Hodgkinson <james@terminaloutcomes.com>
2024-07-24 18:10:55 -0700

    Ubuntu/Debian buildy scripty tweaky things (#2928)
    * updating container builder
    * tweaking dependency script
    * closes #2749 - updates the book for install

--- platform/debian/ubuntu_docker_builder.sh
+++ platform/debian/ubuntu_docker_builder.sh
@@ -2,6 +2,10 @@
 
 # Starts a ubuntu docker container with the source code mounted
 
+if [ -z "${KANIDM_CONTAINER}" ]; then
+    KANIDM_CONTAINER="ubuntu:latest"
+fi
+
 if [ "$(basename "$(pwd)")" != "kanidm" ]; then
     echo "Please run this from the root dir of the repo"
     exit 1
@@ -9,10 +13,14 @@ fi
 
 echo "Starting base ubuntu container"
 echo "Repository is in ~/kanidm/"
-docker run --rm -it \
+
+# shellcheck disable=SC2068
+# shellcheck disable=SC2086
+docker run --rm -it $KANIDM_BUILDER_OPTIONS \
     -e "INSTALL_RUST=1" \
     -e "PACKAGING=1" \
+    -e "TZ=UTC" \
     -v "$(pwd):/root/kanidm/" \
     --workdir "/root/kanidm/" \
     --entrypoint "/root/kanidm/platform/debian/interactive_entrypoint.sh" \
-    ubuntu:latest "$@"
+    "${KANIDM_CONTAINER}" $@
