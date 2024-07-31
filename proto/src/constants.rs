861176d6315d04dd06347c8de443e8d995fe2e22
Sebastiano Tocci <sebastiano.tocci@proton.me>
2024-07-31 16:01:38 +0200

    fixed wording

--- proto/src/constants.rs
+++ proto/src/constants.rs
@@ -66,7 +66,7 @@ pub const MAX_WRITE_THROUGHPUT_BY_TASK_POLL_INTERVAL: [(u64, u64); 13] = [
     (15893, 15),
 ];
 
-// This represents the percentage of the current MAX_WRITE_THROUGHPUT (determined by the current replication interval)
+// This represents the ratio of the current MAX_WRITE_THROUGHPUT (determined by the current replication interval)
 // above which we will not trigger replication as we are too close to the maximum throughput and we risk to lower the server performance
 pub const REPLICATION_TRIGGER_SAFETY_THRESHOLD: f64 = 0.8;
 
