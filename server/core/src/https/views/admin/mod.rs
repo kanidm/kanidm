1a941128f397fb7f26eeafdde30d25e4ed1cbd1f
ToxicMushroom <32853531+ToxicMushroom@users.noreply.github.com>
2024-09-03 03:09:35 +0200

    More scaffolding for groups

--- server/core/src/https/views/admin/mod.rs
+++ server/core/src/https/views/admin/mod.rs
@@ -1,5 +1,5 @@
 use crate::https::ServerState;
-use axum::routing::get;
+use axum::routing::{get, post};
 use axum::Router;
 use axum_htmx::HxRequestGuardLayer;
 
@@ -12,7 +12,12 @@ pub fn admin_router() -> Router<ServerState> {
         .route("/", get(admin::view_admin_get))
         .route("/accounts", get(accounts::view_accounts_get))
         .route("/groups", get(groups::view_groups_get))
-        .route("/group/create", get(groups::view_group_create_get));
+        .route("/groups/unlock", get(groups::view_groups_unlock_get))
+        .route("/group/create", get(groups::view_group_create_get))
+        .route("/group/:group_uuid/delete", post(groups::view_group_delete_post))
+        .route("/group/:group_uuid/edit", get(groups::view_group_edit_get))
+        .route("/group/:group_uuid/view", get(groups::view_group_view_get))
+        .route("/group/create", post(groups::view_group_create_post));
 
     let guarded_router = Router::new()
         .layer(HxRequestGuardLayer::new("/ui"));
