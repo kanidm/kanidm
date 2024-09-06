46da027bfaa306da9cc40fe560c6116347bb76b5
Wei Jian Gan <weijiangan@outlook.com>
2024-09-06 23:58:30 +0800

    add serde rename and de/serialize attributes

--- server/core/src/https/views/constants.rs
+++ server/core/src/https/views/constants.rs
@@ -1,4 +1,7 @@
-#[derive(PartialEq, Clone)]
+use serde::{Deserialize, Serialize};
+
+#[derive(PartialEq, Clone, Deserialize, Serialize)]
+#[serde(rename_all = "snake_case")]
 pub(crate) enum ProfileMenuItems {
     UserProfile,
     SshKeys,
