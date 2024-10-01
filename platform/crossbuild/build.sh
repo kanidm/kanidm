634907bc83c93cea9acf1fc75ef663bb83e787f0
Jinna Kiisuo <jinnak@nocturnal.fi>
2024-10-01 21:40:29 +0300

    refactor: Rework crossbuild to be more generic
    - Also fixes issues with OpenSSL cross-building.

--- platform/crossbuild/build.sh
+++ platform/crossbuild/build.sh
@@ -6,7 +6,7 @@ if [ -z "$1" ]; then
     echo "Usage: $0 target_os"
     if [ -d ./platform ]; then
         echo "Options:"
-        find platform/crossbuild -type d -maxdepth 1 -mindepth 1 | awk -F'/' '{print $NF}' | sort
+        find platform/crossbuild -maxdepth 1 -mindepth 1 -type d | awk -F'/' '{print $NF}' | sort
     fi
     exit 1
 fi
@@ -16,11 +16,9 @@ if [ ! -d "platform/crossbuild/$1" ]; then
     exit 1
 fi
 
-TARGET_DIR="./target/$1"
-
-echo "Recreating then building to ${TARGET_DIR}"
-rm -rf "${TARGET_DIR}"
-mkdir -p "${TARGET_DIR}"
+# Find the target rust architecture
+TRIPLET=$(echo $1 | cut -d \- -f 3-)
+echo "Crossbuilding for: $TRIPLET"
 
 CROSS_CONFIG="platform/crossbuild/${1}/Cross.toml" \
     cross build --target aarch64-unknown-linux-gnu \
@@ -35,15 +33,9 @@ CROSS_CONFIG="platform/crossbuild/${1}/Cross.toml" \
         -p nss_kanidm \
         --release
 
-find "./target/aarch64-unknown-linux-gnu/release/" -maxdepth 1 \
+TRIPLET=$(echo $1 | cut -d \- -f 3-)
+
+echo "Build artefacts for ${TRIPLET}:"
+find "./target/${TRIPLET}/release/" -maxdepth 1 \
     -type f -not -name '*.d' \
-    -name 'kanidm*' \
-    -exec mv "{}" "${TARGET_DIR}/" \;
-
-find "./target/aarch64-unknown-linux-gnu/release/" -maxdepth 1 \
-    -name '*kanidm*.so' \
-    -exec mv "{}" "${TARGET_DIR}/" \;
-# find "${TARGET_DIR}" -name '*.d' -delete
-
-echo "Contents of ${TARGET_DIR}"
-find "${TARGET_DIR}" -type f
+    -name '*kanidm*'
