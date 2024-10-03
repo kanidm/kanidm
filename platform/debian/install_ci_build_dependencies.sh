88b820157fa20e58e8df7456eeabab7b71c2493e
Jinna Kiisuo <jinnak@nocturnal.fi>
2024-10-03 18:10:49 +0300

    fix: Work around more GHA weirdness in apt sources

--- platform/debian/install_ci_build_dependencies.sh
+++ platform/debian/install_ci_build_dependencies.sh
@@ -4,18 +4,23 @@
 # This script is for GitHub Actions and makes a bunch of assumptions you dear human will struggle with.
 # This script is actively hostile against you human, you have been warned. :3
 
+if [[ -z "$CI" ]]; then
+    >&2 echo "Error, this script is only to be run from CI."
+    exit 1
+fi
+
 set -eu
 
 DEB_OS="${1?}" # Expecting "debian" or "ubuntu"
 DEB_ARCH="${2?}" # Expecting "amd64" or "arm64" etc
 
 # Ubuntu does not do multiarch correctly, does not document this at all etc,
-# so we just need a bunch of dirty hacks to do it Their Way.
+# so we just need a bunch of dirty hacks to do it Their Way. GitHub Actions makes this even worse.
 # Technically we don't need to do this with a native build, but it also doesn't hurt.
 if [[ "$DEB_OS" == "ubuntu" ]]; then
     2>&1 echo "Patching Ubuntu apt sources for multiarch"
     # Default entries do not pin an arch, fix that
-    sudo sed 's/^deb http/deb [arch=amd64] http/' -i '/etc/apt/sources.list'
+    sudo sed -E 's/^deb (http|mirror)/deb [arch=amd64] \1/' -i '/etc/apt/sources.list'
     # arm64 is on a completely different mirror structure, add that pinned to arm64
     echo 'deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports/ jammy main restricted' | sudo tee /etc/apt/sources.list.d/arm64.list
     echo 'deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports/ jammy-updates main restricted' | sudo tee -a /etc/apt/sources.list.d/arm64.list
