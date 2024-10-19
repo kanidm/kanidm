8c30bf88d93f221fb994910798fbf22945f35896
Water261 <hello@water261.au>
2024-10-20 09:09:05 +1000

    Mermaid theme changes based on mdbook theme

--- book/mermaid-init.js
+++ book/mermaid-init.js
@@ -1,1 +1,12 @@
-mermaid.initialize({startOnLoad:true});
+//* Changes mermaid theme based on selected mdbook theme
+const rootElement = document.documentElement;
+const darkThemes = ["coal", "navy", "ayu"];
+// Get the intersection of the HTML element class list & mdbook's dark themes
+const selectedDarkTheme = [...rootElement.classList].filter((c) => darkThemes.includes(c));
+
+if (selectedDarkTheme.length > 0) {
+	mermaid.initialize({ startOnLoad: true, theme: "dark" });
+} else {
+	mermaid.initialize({ startOnLoad: true, theme: "default" });
+}
+
