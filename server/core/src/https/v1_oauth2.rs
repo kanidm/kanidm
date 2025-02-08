6445c28fe7b28198dffcfdbc8042c27c5014b0b6
CEbbinghaus <git@cebbinghaus.com>
2025-02-08 19:49:01 +1100

    Removed API check for empty scopemaps

--- server/core/src/https/v1_oauth2.rs
+++ server/core/src/https/v1_oauth2.rs
@@ -201,10 +201,6 @@ pub(crate) async fn oauth2_id_scopemap_post(
 ) -> Result<Json<()>, WebError> {
     let filter = oauth2_id(&rs_name);
 
-    if scopes.is_empty() {
-        return Err(WebError::OperationError(OperationError::EmptyRequest));
-    }
-
     state
         .qe_w_ref
         .handle_oauth2_scopemap_update(client_auth_info, group, scopes, filter, kopid.eventid)
