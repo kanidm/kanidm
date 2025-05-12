54caad2f21c248ce56fe7a61fd9e64049565483f
James Hodgkinson <james@terminaloutcomes.com>
2025-05-12 10:27:42 +1000

    maint: removing rg from test scripts

--- scripts/setup_dev_environment.sh
+++ scripts/setup_dev_environment.sh
@@ -45,7 +45,7 @@ fi
 
 # defaults
 KANIDM_CONFIG_FILE="./insecure_server.toml"
-KANIDM_URL="$(rg origin "${KANIDM_CONFIG_FILE}" | awk '{print $NF}' | tr -d '"')"
+KANIDM_URL="$(grep origin "${KANIDM_CONFIG_FILE}" | awk '{print $NF}' | tr -d '"')"
 KANIDM_CA_PATH="/tmp/kanidm/ca.pem"
 
 # wait for them to shut down the server if it's running...
