87fb066aff167d8f62eaf255bbc2f5d0513e851b
Firstyear <william@blackhats.net.au>
2025-05-21 18:16:03 +1000

    Swap bytes mut at buffer limits (#3651)
    * Swap bytes mut at buffer limits

--- unix_integration/common/src/constants.rs
+++ unix_integration/common/src/constants.rs
@@ -23,3 +23,9 @@ pub const DEFAULT_SHELL_SEARCH_PATHS: &[&str] = &["/bin"];
 
 #[cfg(all(target_family = "unix", target_os = "freebsd"))]
 pub const DEFAULT_SHELL_SEARCH_PATHS: &[&str] = &["/bin", "/usr/local/bin"];
+
+// The minimum size of a buffer for the unix stream codec
+pub const CODEC_MIMIMUM_BYTESMUT_ALLOCATION: usize = 64;
+// If the codec buffer exceeds this limit, then we swap the buffer
+// with a fresh one to prevent memory explosions.
+pub const CODEC_BYTESMUT_ALLOCATION_LIMIT: usize = 1024 * 1024;
