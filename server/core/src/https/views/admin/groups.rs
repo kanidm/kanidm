0d1540c2c1f7ff7537720f844992146966a810e9
ToxicMushroom <32853531+ToxicMushroom@users.noreply.github.com>
2025-08-12 05:45:07 +0200

    Add webui for adding members to groups

--- server/core/src/https/views/admin/groups.rs
+++ server/core/src/https/views/admin/groups.rs
@@ -3,7 +3,7 @@ use crate::https::extractors::{DomainInfo, VerifiedClientInformation};
 use crate::https::middleware::KOpId;
 use crate::https::views::errors::HtmxError;
 use crate::https::views::navbar::NavbarCtx;
-use crate::https::views::Urls;
+use crate::https::views::{ErrorToastPartial, Urls};
 use crate::https::ServerState;
 use askama::Template;
 use axum::extract::{Path, State};
@@ -21,14 +21,19 @@ use kanidm_proto::scim_v1::server::{
 use kanidm_proto::scim_v1::ScimEntryGetQuery;
 use kanidm_proto::scim_v1::{client::ScimEntryPutKanidm, ScimFilter};
 use kanidmd_lib::constants::EntryClass;
+use kanidmd_lib::filter::{f_eq, Filter};
 use kanidmd_lib::idm::ClientAuthInfo;
 use serde::{Deserialize, Serialize};
 use std::collections::BTreeMap;
 use std::str::FromStr;
 use uuid::Uuid;
 
-pub const GROUP_ATTRIBUTES: [Attribute; 3] =
-    [Attribute::Uuid, Attribute::Name, Attribute::Description];
+pub const GROUP_ATTRIBUTES: [Attribute; 4] = [
+    Attribute::Uuid,
+    Attribute::Name,
+    Attribute::Description,
+    Attribute::Member,
+];
 
 #[derive(Template)]
 #[template(path = "admin/admin_panel_template.html")]
@@ -56,6 +61,28 @@ struct GroupViewPartial {
     group: ScimGroup,
     can_rw: bool,
     scim_effective_access: ScimEffectiveAccess,
+}
+
+// #[derive(Template)]
+// #[template(path = "admin/admin_group_member_entry_partial.html")]
+// struct GroupMemberEntryPartial {
+//     group_uuid: Uuid,
+//     member_name: String,
+//     can_edit_member: bool,
+// }
+
+#[derive(Template)]
+#[template(
+    ext = "html",
+    source = "\
+(% include \"admin/admin_group_member_entry_partial.html\" %)\
+(% include \"admin/saved_toast.html\" %)\
+"
+)]
+struct GroupMemberEntryResponse {
+    group_uuid: Uuid,
+    member_name: String,
+    can_edit_member: bool,
 }
 
 #[derive(Template)]
@@ -226,12 +253,160 @@ pub(crate) async fn edit_group(
         id: group_uuid,
         attrs,
     }
-    .try_into()
-    .map_err(|_| HtmxError::new(&kopid, OperationError::Backend, domain_info.clone()))?;
+        .try_into()
+        .map_err(|_| HtmxError::new(&kopid, OperationError::Backend, domain_info.clone()))?;
 
     state
         .qe_w_ref
         .handle_scim_entry_put(client_auth_info.clone(), kopid.eventid, generic)
+        .map_err(|op_err| HtmxError::new(&kopid, op_err, domain_info.clone()))
+        .await?;
+
+    // return floating notification: saved/failed
+    Ok((SavedToast {}).into_response())
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub(crate) struct AddMemberForm {
+    member: String,
+}
+
+pub(crate) async fn add_member(
+    State(state): State<ServerState>,
+    Extension(kopid): Extension<KOpId>,
+    DomainInfo(domain_info): DomainInfo,
+    VerifiedClientInformation(client_auth_info): VerifiedClientInformation,
+    Path(group_uuid): Path<Uuid>,
+    // Form must be the last parameter because it consumes the request body
+    Form(query): Form<AddMemberForm>,
+) -> axum::response::Result<Response> {
+    let filter = filter_all!(f_eq(Attribute::Class, EntryClass::Group.into()));
+
+    let get_query = ScimEntryGetQuery {
+        attributes: Some(vec![Attribute::Member]),
+        ext_access_check: false,
+        sort_by: None,
+        sort_order: None,
+        start_index: None,
+        count: None,
+        filter: None,
+    };
+    let get_member_query = ScimEntryGetQuery {
+        attributes: Some(vec![Attribute::Spn]),
+        ext_access_check: false,
+        sort_by: None,
+        sort_order: None,
+        start_index: None,
+        count: None,
+        filter: None,
+    };
+    let group_uuid_str = String::from(group_uuid);
+    let before = state
+        .qe_r_ref
+        .scim_entry_id_get(
+            client_auth_info.clone(),
+            kopid.eventid,
+            group_uuid_str.clone(),
+            EntryClass::Group,
+            get_query.clone(),
+        )
+        .map_err(|op_err| HtmxError::new(&kopid, op_err, domain_info.clone()))
+        .await?;
+    state
+        .qe_w_ref
+        .handle_appendattribute(
+            client_auth_info.clone(),
+            group_uuid_str.clone(),
+            "member".to_string(),
+            vec![query.member.clone()],
+            filter,
+            kopid.eventid,
+        )
+        .map_err(|op_err| HtmxError::new(&kopid, op_err, domain_info.clone()))
+        .await?;
+
+    let after = state
+        .qe_r_ref
+        .scim_entry_id_get(
+            client_auth_info.clone(),
+            kopid.eventid,
+            group_uuid_str.clone(),
+            EntryClass::Group,
+            get_query,
+        )
+        .map_err(|op_err| HtmxError::new(&kopid, op_err, domain_info.clone()))
+        .await?;
+
+    let before_len = if let Some(ScimValueKanidm::EntryReferences(members_before)) =
+        before.attrs.get(&Attribute::Member)
+    {
+        members_before.len()
+    } else {
+        0
+    };
+    let after_len = if let Some(ScimValueKanidm::EntryReferences(members_after)) =
+        after.attrs.get(&Attribute::Member)
+    {
+        members_after.len()
+    } else {
+        0
+    };
+
+
+    if before_len + 1 == after_len {
+        let added_member_scim = state.qe_r_ref.scim_entry_id_get(
+            client_auth_info.clone(),
+            kopid.eventid,
+            query.member.clone(),
+            EntryClass::Object,
+            get_member_query)
+            .map_err(|op_err| HtmxError::new(&kopid, op_err, domain_info.clone()))
+            .await?;
+
+        let Some(ScimValueKanidm::String(added_member_spn)) = added_member_scim.attrs.get(&Attribute::Spn) else {
+            return Ok((ErrorToastPartial {
+                err_code: OperationError::UI0004MemberAlreadyExists,
+                operation_id: kopid.eventid,
+            }).into_response())
+        };
+        // New entry + saved toast.
+        Ok((GroupMemberEntryResponse {
+            group_uuid,
+            member_name: added_member_spn.to_string(),
+            can_edit_member: true,
+        })
+            .into_response())
+    } else {
+        // Duplicate entry toast.
+        Ok((ErrorToastPartial {
+            err_code: OperationError::UI0004MemberAlreadyExists,
+            operation_id: kopid.eventid,
+        })
+            .into_response())
+    }
+}
+
+pub(crate) async fn remove_member(
+    State(state): State<ServerState>,
+    Extension(kopid): Extension<KOpId>,
+    DomainInfo(domain_info): DomainInfo,
+    VerifiedClientInformation(client_auth_info): VerifiedClientInformation,
+    Path(group_uuid): Path<Uuid>,
+    // Form must be the last parameter because it consumes the request body
+    Form(query): Form<AddMemberForm>,
+) -> axum::response::Result<Response> {
+    let filter = filter_all!(f_eq(Attribute::Class, EntryClass::Group.into()));
+
+    state
+        .qe_w_ref
+        .handle_removeattributevalues(
+            client_auth_info.clone(),
+            String::from(group_uuid),
+            "member".to_string(),
+            vec![query.member],
+            filter,
+            kopid.eventid,
+        )
         .map_err(|op_err| HtmxError::new(&kopid, op_err, domain_info.clone()))
         .await?;
 
