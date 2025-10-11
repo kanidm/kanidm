c685308c67c4f87c71f7b3a8594a662a97fda3a4
James Hodgkinson <james@terminaloutcomes.com>
2025-10-12 09:26:49 +1000

    fix: clippy lints

--- server/core/src/https/apidocs/path_schema.rs
+++ server/core/src/https/apidocs/path_schema.rs
@@ -1,9 +1,1 @@
 //! Path schema objects for the API documentation.
-
-use serde::{Deserialize, Serialize};
-use utoipa::IntoParams;
-
-#[derive(IntoParams, Serialize, Deserialize, Debug)]
-pub(crate) struct UuidOrName {
-    id: String,
-}
