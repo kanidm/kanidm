82e82694148d6dfd8a0046f9539a9e8b60a85322
James Hodgkinson <james@terminaloutcomes.com>
2026-02-28 15:29:25 +1000

    header crimes

---
+++ unix_integration/rlm_kanidm/pull_headers.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+CURRDIR="$(pwd)"
+
+mkdir -p /tmp/freeradius-src mkdir -p /usr/include/freeradius-devel/
+curl -fsSL https://github.com/FreeRADIUS/freeradius-server/archive/refs/tags/release_3_2_8.tar.gz | tar -xz -C /tmp/freeradius-src
+mv /tmp/freeradius-src/freeradius-server-release{*,}
+cd /tmp/freeradius-src/freeradius-server-release
+./configure --with-openssl=no > /tmp/freeradius_configure.log 2>&1
+make > /tmp/freeradius_make.log 2>&1
+mkdir -p /usr/include/freeradius/
+cp -R ./src/include/* /usr/include/freeradius/
+cp -R ./src/include/* /usr/include/freeradius-devel/
+
+echo "Done! Moving back to $CURRDIR"
+cd "$CURRDIR"
