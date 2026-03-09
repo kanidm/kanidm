90ff80b9f9168c74dcc53c613a0ac46c721a554d
CEbbinghaus <git@cebbinghaus.com>
2026-03-09 15:25:29 +1100

    Removed phantom attribute and made it real
    # Conflicts:
    #	server/lib/src/constants/uuids.rs
    #	server/lib/src/migration_data/dl13/mod.rs

--- server/lib/src/migration_data/dl13/schema.rs
+++ server/lib/src/migration_data/dl13/schema.rs
@@ -1031,6 +1031,14 @@ pub static SCHEMA_ATTR_S256: LazyLock<SchemaAttribute> = LazyLock::new(|| Schema
     ..Default::default()
 });
 
+pub static ref SCHEMA_ATTR_PASSWORD_CHANGED_TIME: SchemaAttribute = SchemaAttribute {
+    uuid: UUID_SCHEMA_ATTR_PASSWORD_CHANGED_TIME,
+    name: Attribute::PasswordChangedTime,
+    description: "The time when the password was last changed.".to_string(),
+    syntax: SyntaxType::DateTime,
+    ..Default::default()
+};
+
 // === classes ===
 pub static SCHEMA_CLASS_PERSON_DL8: LazyLock<SchemaClass> = LazyLock::new(|| SchemaClass {
     uuid: UUID_SCHEMA_CLASS_PERSON,
