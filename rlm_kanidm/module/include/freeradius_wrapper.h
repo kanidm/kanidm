a99d2365d28dfb025774974aa014de4b53815e5d
William Brown <william@blackhats.net.au>
2026-03-25 12:50:27 +1000

    Refactor underway

--- rlm_kanidm/module/include/freeradius_wrapper.h
+++ rlm_kanidm/module/include/freeradius_wrapper.h
@@ -35,18 +35,5 @@ typedef struct rlm_kanidm_conf_parser_t {
     void const *dflt;
 } rlm_kanidm_conf_parser_t;
 
-typedef int (*rlm_kanidm_instantiate_t)(void *mod_cs, void *instance);
-typedef int (*rlm_kanidm_detach_t)(void *instance);
-typedef rlm_rcode_t (*rlm_kanidm_packetmethod_t)(void *instance, REQUEST *request);
 
-typedef struct rlm_kanidm_module_t {
-    uint64_t magic;
-    char const *name;
-    int type;
-    size_t inst_size;
-    rlm_kanidm_conf_parser_t const *config;
-    rlm_kanidm_instantiate_t bootstrap;
-    rlm_kanidm_instantiate_t instantiate;
-    rlm_kanidm_detach_t detach;
-    rlm_kanidm_packetmethod_t methods[RLM_KANIDM_MOD_COUNT];
-} rlm_kanidm_module_t;
+
---
+++ rlm_kanidm/module/src/error.rs
@@ -0,0 +1,29 @@
+
+#[derive(Debug)]
+pub enum ModuleError {
+    Io(String),
+    Config(String),
+    Http(String),
+    Other(String),
+}
+
+impl std::fmt::Display for ModuleError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Io(s) => write!(f, "IO Error: {s}"),
+            Self::Config(s) => write!(f, "Config Error: {s}"),
+            Self::Http(s) => write!(f, "HTTP Error: {s}"),
+            Self::Other(s) => write!(f, "Internal Error: {s}"),
+        }
+    }
+}
+
+impl From<ModuleError> for AuthResultC {
+    fn from(input: ModuleError) -> AuthResultC {
+        auth_error(&Response::Fail, input.to_string())
+    }
+}
+
+impl std::error::Error for ModuleError {}
+
+
