6b1dc9f652bdfb786ee3dfdf7caa96e17cca22c6
William Brown <william@blackhats.net.au>
2026-05-09 14:55:28 +1000

    Progress

--- libs/templating/src/grammar.rs
+++ libs/templating/src/grammar.rs
@@ -2,10 +2,10 @@ use std::str::FromStr;
 
 peg::parser! {
     grammar template() for str {
-        pub rule parse<A: FromStr, C: FromStr, O: FromStr>() -> Vec<TemplateIntermediate<A>> =
+        pub rule parse<A: FromStr, C: FromStr, O: FromStr>() -> Vec<TemplateIntermediate<A, C, O>> =
             s:(element()*) { s }
 
-        rule element<A: FromStr>() -> TemplateIntermediate<A> = precedence!{
+        rule element<A: FromStr, C: FromStr, O: FromStr>() -> TemplateIntermediate<A, C, O> = precedence!{
             start_template() o:operand::<A, C, O>() end_template()
                 { o }
             --
@@ -13,7 +13,7 @@ peg::parser! {
                 {  TemplateIntermediate::Literal (s) }
         }
 
-        rule operand<A: FromStr>() -> TemplateIntermediate<A> =
+        rule operand<A: FromStr, C: FromStr, O: FromStr>() -> TemplateIntermediate<A, C, O> =
             separator()* a:attrname::<A>() separator()* c:condition::<C>()? separator()* o:option::<O>()? separator()*
                 { TemplateIntermediate::Operand {
                     attribute: a,
@@ -21,31 +21,29 @@ peg::parser! {
                     options: o }
                 }
 
-        rule condition() -> TemplateCondition =
+        rule condition<C: FromStr>() -> C =
             start_condition() separator()+ c:condition_str()
                 { c }
 
-        rule condition_str() -> TemplateCondition = precedence!{
-            "A"
-            { TemplateCondition::A }
-        }
+        rule condition_str<C: FromStr>() -> C =
+            s:$(['a'..='z' | 'A'..='Z' | '0'..='9' | '-' | '_' | '(' | ')']+)
+                {? C::from_str(s).or(Err("invalid condition")) }
 
-        rule option() -> TemplateOption =
+        rule option<O: FromStr>() -> O =
             start_option() separator()+ os:option_str()
                 { os }
 
-        rule option_str() -> TemplateOption = precedence!{
-            "json"
-            { TemplateOption::FormatJson }
-        }
-
+        rule option_str<O: FromStr>() -> O =
+            s:$(['a'..='z']+)
+                {? O::from_str(s).or(Err("invalid option")) }
 
         rule literal() -> String =
             s:$((!start_template()[_])+)
             { s.to_string() }
 
         rule attrname<A: FromStr>() -> A =
-            s:$([ 'a'..='z' | 'A'..='Z']['a'..='z' | 'A'..='Z' | '0'..='9' | '-' | '_' ]*) {? A::from_str(s).or(Err("invalid attribute name")) }
+            s:$([ 'a'..='z' | 'A'..='Z']['a'..='z' | 'A'..='Z' | '0'..='9' | '-' | '_']*)
+                {? A::from_str(s).or(Err("invalid attribute name")) }
 
         rule start_condition() =
             ['i']['f']
@@ -75,22 +73,36 @@ enum TemplateCondition {
 }
 
 #[derive(Debug)]
-enum TemplateIntermediate<A> {
-    // None,
+enum TemplateAttribute {
+    Ident,
+}
+
+impl FromStr for TemplateAttribute {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "ident" => Ok(Self::Ident),
+            _ => Err(()),
+        }
+    }
+}
+
+#[derive(Debug)]
+enum TemplateIntermediate<A, C, O> {
     Literal(String),
-    // Join(Self, Self),
     Operand {
         attribute: A,
-        condition: Option<TemplateCondition>,
-        options: Option<TemplateOption>,
+        condition: Option<C>,
+        options: Option<O>,
     },
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{template, TemplateIntermediate};
+    use super::{template, TemplateAttribute, TemplateIntermediate};
 
-    type TemplateTest = Vec<TemplateIntermediate<String>>;
+    type TemplateTest = Vec<TemplateIntermediate<String, String, String>>;
 
     #[test]
     fn single_literal() {
@@ -161,6 +173,24 @@ mod tests {
         let _ = tracing_subscriber::fmt::try_init();
 
         let x: TemplateTest = template::parse("{{ ident | json }}").unwrap();
+
+        tracing::trace!(?x);
+    }
+
+    #[test]
+    fn operand_condition_option_json() {
+        let _ = tracing_subscriber::fmt::try_init();
+
+        let x: TemplateTest = template::parse("{{ ident if memberof(abc) | json }}").unwrap();
+
+        tracing::trace!(?x);
+    }
+
+    #[test]
+    fn operand_condition() {
+        let _ = tracing_subscriber::fmt::try_init();
+
+        let x: TemplateTest = template::parse("{{ ident if memberof(abc) }}").unwrap();
 
         tracing::trace!(?x);
     }
