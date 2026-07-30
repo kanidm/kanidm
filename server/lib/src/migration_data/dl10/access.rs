23853f27d145d40b22d083f66ccece506d049b60
William Brown <william@firstyear.id.au>
2026-07-30 13:53:28 +1000

    Comment

--- server/lib/src/migration_data/dl10/access.rs
+++ server/lib/src/migration_data/dl10/access.rs
@@ -1,7 +1,6 @@
 #![allow(clippy::expect_used)]
 //! Constant Entries for the IDM
 
-// use crate::constants::uuids::*;
 use crate::entry::EntryInitNew;
 use crate::prelude::*;
 use crate::value::Value;
