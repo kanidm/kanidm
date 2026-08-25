1158d52a906fb2ca7e9be656a851221d9bbe5c25
William Brown <william@firstyear.id.au>
2026-08-25 15:29:05 +1000

    Resolve clippy errors.

--- tools/cli/src/cli/common.rs
+++ tools/cli/src/cli/common.rs
@@ -11,7 +11,7 @@ use crate::session::{process_auth_state, read_tokens};
 use crate::{KanidmClientParser, LoginOpt};
 
 #[derive(Debug)]
-#[allow(clippy::large_enum_variant, clippy::result_large_err)]
+#[allow(clippy::large_enum_variant)]
 pub enum ToClientError {
     NeedLogin(String),
     NeedReauth(String, KanidmClient),
@@ -111,6 +111,7 @@ impl KanidmClientParser {
         })
     }
 
+    #[allow(clippy::result_large_err)]
     pub(crate) async fn try_to_client(
         &self,
         optype: OpType,
